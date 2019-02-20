f83d1dab9f03ad03e8a8af2b952de72b25898675
Evan <evan@beatsperminute.com>
2019-02-19 23:54:46 -0500

    tsc

--- stores/InvoicesStore.ts
+++ stores/InvoicesStore.ts
@@ -12,7 +12,7 @@ export default class InvoicesStore {
     @observable getPayReqError: boolean = false;
     @observable invoices: Array<Invoice> = [];
     @observable invoice: Invoice;
-    @observable pay_req: PaymentRequest | null;
+    @observable pay_req: PaymentRequest;
     @observable payment_request: string | null;
     @observable creatingInvoice: boolean = false;
     @observable creatingInvoiceError: boolean = false;
@@ -130,7 +130,6 @@ export default class InvoicesStore {
             // handle error
             this.loading = false;
             this.getPayReqError = true;
-            this.pay_req = null;
         });
     }
 }
