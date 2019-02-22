029c22a7727d47f6d3a89ef39eff2ab53ab75bbe
Evan <evan@beatsperminute.com>
2019-02-22 00:25:01 -0500

    Design: tighten up button margins

--- views/Wallet/MainPane.tsx
+++ views/Wallet/MainPane.tsx
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 30,
+        marginTop: 20,
         marginBottom: -30
     }
 });
