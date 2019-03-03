d69d388aca40f99fec9ddbaf6e22e210fb3bd434
Evan <evan@beatsperminute.com>
2019-03-03 17:46:13 -0500

    Bug fix: lndconnect macaroon is in Base64URL format
    I was assuming it was in ASCII

--- utils/MacaroonUtils.ts
+++ utils/MacaroonUtils.ts
@@ -1,12 +1,34 @@
+import { decode as atob } from 'base-64';
+
 class MacaroonUtils {
-    asciiToHex = (input: string) => {
-        const arr1: Array<string> = [];
-      	for (let n = 0, l = input.length; n < l; n++) {
-        		const hex: string = Number(input.charCodeAt(n)).toString(16);
-        		arr1.push(hex);
-      	}
-      	return arr1.join('');
-    };
+    base64UrlToHex = (input: string) => {
+        const raw = atob(this.base64UrlToBase64(input));
+        let HEX = '';
+
+        for (let i = 0; i < raw.length; i++) {
+            const hexChar = raw.charCodeAt(i).toString(16);
+            HEX += (hexChar.length === 2 ? hexChar : '0' + hexChar);
+        }
+        return HEX.toUpperCase();
+    }
+
+    base64UrlToBase64 = (input: string) => {
+        // Replace non-url compatible chars with base64 standard chars
+        input = input
+            .replace(/-/g, '+')
+            .replace(/_/g, '/');
+
+        // Pad out with standard base64 required padding characters
+        var pad = input.length % 4;
+        if(pad) {
+          if(pad === 1) {
+            throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+          }
+          input += new Array(5-pad).join('=');
+        }
+
+        return input;
+    }
 };
 
 const macaroonUtils = new MacaroonUtils();
