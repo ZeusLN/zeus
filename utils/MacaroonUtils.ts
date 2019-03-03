51a6bdfa01030466e4aa3cf27322b9113bcad849
Evan <evan@beatsperminute.com>
2019-03-03 17:47:40 -0500

    linting

--- utils/MacaroonUtils.ts
+++ utils/MacaroonUtils.ts
@@ -19,12 +19,12 @@ class MacaroonUtils {
             .replace(/_/g, '/');
 
         // Pad out with standard base64 required padding characters
-        var pad = input.length % 4;
-        if(pad) {
-          if(pad === 1) {
-            throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
-          }
-          input += new Array(5-pad).join('=');
+        const pad = input.length % 4;
+        if (pad) {
+            if (pad === 1) {
+                throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+            }
+            input += new Array(5-pad).join('=');
         }
 
         return input;
