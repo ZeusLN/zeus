d8c4308ec16aea8aa6ad10a1ea7761282a9efa4d
Evan <evan@beatsperminute.com>
2019-03-04 20:04:30 -0500

    Design/Bug Fix: Remove padding between button group and lists on the main Wallet view
    Hopefully this fixes some related scrolling issues where some users are unable to press items at the end of lists

--- views/Wallet/Invoices.tsx
+++ views/Wallet/Invoices.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import { Button, List, ListItem } from 'react-native-elements';
+import { Button, ListItem } from 'react-native-elements';
 import Invoice from './../../models/Invoice';
 import { inject, observer } from 'mobx-react';
 
@@ -30,31 +30,29 @@ export default class InvoicesView extends React.Component<InvoicesProps, {}> {
 
         return (
             <View style={{ flex: 1 }}>
-                {(!!invoices && invoices.length > 0) || loading  ? <List>
-                        <FlatList
-                            data={invoices}
-                            renderItem={({ item }) => {
-                                const settleDate = new Date(Number(item.settle_date) * 1000).toString();
-                                const creationDate = new Date(Number(item.creation_date) * 1000).toString();
-                                const { settled } = item;
-                                return (
-                                    <ListItem
-                                        key={item.r_hash}
-                                        title={item.memo || "No memo"}
-                                        subtitle={`${settled ? 'Paid' : 'Unpaid'}: ${units && getAmount(item.value)} | ${settled ? settleDate : creationDate}`}
-                                        containerStyle={{ borderBottomWidth: 0 }}
-                                        avatar={item.settled ? AddBalance : AddBalancePending}
-                                        onPress={() => navigation.navigate('Invoice', { invoice: item })}
-                                    />
-                                );
-                            }}
-                            keyExtractor={item => item.r_hash}
-                            ItemSeparatorComponent={this.renderSeparator}
-                            onEndReachedThreshold={50}
-                            refreshing={loading}
-                            onRefresh={() => refresh()}
-                        />
-                </List> : <Button
+                {(!!invoices && invoices.length > 0) || loading  ? <FlatList
+                    data={invoices}
+                    renderItem={({ item }) => {
+                        const settleDate = new Date(Number(item.settle_date) * 1000).toString();
+                        const creationDate = new Date(Number(item.creation_date) * 1000).toString();
+                        const { settled } = item;
+                        return (
+                            <ListItem
+                                key={item.r_hash}
+                                title={item.memo || "No memo"}
+                                subtitle={`${settled ? 'Paid' : 'Unpaid'}: ${units && getAmount(item.value)} | ${settled ? settleDate : creationDate}`}
+                                containerStyle={{ borderBottomWidth: 0 }}
+                                avatar={item.settled ? AddBalance : AddBalancePending}
+                                onPress={() => navigation.navigate('Invoice', { invoice: item })}
+                            />
+                        );
+                    }}
+                    keyExtractor={item => item.r_hash}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    onEndReachedThreshold={50}
+                    refreshing={loading}
+                    onRefresh={() => refresh()}
+                /> : <Button
                     title="No Invoices"
                     icon={{
                         name: "error-outline",
