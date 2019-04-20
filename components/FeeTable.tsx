8f155d16f6400e7c6631729dec7d07ef7956ae46
Evan <evan@beatsperminute.com>
2019-04-20 18:21:07 -0400

    Clean-up code

--- components/FeeTable.tsx
+++ components/FeeTable.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import FeeUtils from './../utils/FeeUtils';
 import FeeStore from './../stores/FeeStore';
-import { Cell, DataTable, Header, HeaderCell, Row } from 'react-native-data-table';
+import { Cell, Header, Row } from 'react-native-data-table';
 import { isEmpty } from 'lodash';
 
 interface FeeTableProps {
@@ -11,7 +11,7 @@ interface FeeTableProps {
     dataFrame: any;
     copyText?: string;
     loading: boolean;
-    setFee: func;
+    setFee: (value: string) => void;
 }
 
 interface FeeTableState {
@@ -23,44 +23,30 @@ export default class FeeTable extends React.Component<FeeTableProps, FeeTableSta
         collapsed: true
     }
 
-    styler(x) {
-        if(x == null || isNaN(x)) {
+    styler(x: number|string) {
+        if(x == null || typeof x === 'string') {
             return {
-                "background": "grey",
-                "textAlign": "center",
-                "border": `1px solid rgba(255,255,255,0.5)`
+                "backgroundColor": "white",
+                "height": 53,
+                "minWidth": 53
             }
         };
 
-        const c = FeeUtils.gcolor(x/800);
+        const c: any = FeeUtils.gcolor(x/800);
 
         return {
             "backgroundColor": `hsl(${c[0]}, ${c[1]*100}%, ${c[2]*100}%)`,
             "alignItems": "center",
-            "height": 55
+            "height": 53,
+            "minWidth": 53
         };
     }
 
-    repr(x) {
-        if(x == null || isNaN(x)) {
-            return "NaN";
-        }
+    repr = (x: number) => Math.exp(x/100).toFixed(1);
 
-        const approx: string = Math.exp(x/100);
-        let r: string;
+    reprColumn = (x: number) => (x * 100).toFixed(0) + '%';
 
-        r = Math.exp(x/100).toFixed(1);
-
-        return r;
-    }
-
-    reprColumn(x: number) {
-        return (x * 100).toFixed(0) + '%';
-    }
-
-    reprIndex(x: number) {
-        return x / 6 + 'h';
-    }
+    reprIndex = (x: number) => x / 6 + 'h';
 
     openTable = () => {
         const { feeStore } = this.props;
@@ -79,7 +65,7 @@ export default class FeeTable extends React.Component<FeeTableProps, FeeTableSta
 
     render() {
         const { collapsed } = this.state;
-        const { feeStore, dataFrame, loading, setFee } = this.props;
+        const { dataFrame, loading, setFee } = this.props;
 
         const df = dataFrame;
         let headers: any;
@@ -87,20 +73,22 @@ export default class FeeTable extends React.Component<FeeTableProps, FeeTableSta
 
         if (!isEmpty(df) && !loading) {
             headers = df.columns && df.columns.map((columnName: number, index: number) =>
-                <Cell style={{ backgroundColor: 'white' }} width={60} textStyle={{ color: 'black' }} key={`item-${index}`}>{this.reprColumn(columnName)}</Cell>
+                <Cell style={{ backgroundColor: 'white', minWidth: 53, height: 53 }} textStyle={{ color: 'black' }} key={`item-${index}`}>{this.reprColumn(columnName)}</Cell>
             );
 
-            rows = df.index.map((index: any, i: number) => {
+            rows = df.index.map((index: number, i: number) => {
                 const cells = df.data[i].map((cell: any, k: number) => {
                     const value = this.repr(cell);
                     return (
-                        <Cell key={`cell-${k}`} style={this.styler(cell)}><Text style={{ color: value === "NaN" ? 'black' : 'white' }} onPress={() => setFee(value)}>{value}</Text></Cell>
+                        <TouchableOpacity key={`cell-${k}`} onPress={() => setFee(value)}><Cell style={this.styler(cell)}><Text style={{ color: 'white' }}>{value}</Text></Cell></TouchableOpacity>
                     );
                 });
 
+                const indexText = this.reprIndex(index);
+
                 return (
                     <Row key={`row-${i}`}>
-                        <Cell style={{ backgroundColor: 'white' }} textStyle={{ color: 'black' }}>{this.reprIndex(index)}</Cell>
+                        <Cell style={this.styler(indexText)}><Text style={{ color: 'black' }}>{indexText}</Text></Cell>
                         {cells}
                     </Row>
                 );
@@ -128,8 +116,8 @@ export default class FeeTable extends React.Component<FeeTableProps, FeeTableSta
                     onPress={() => collapsed ? this.openTable() : this.closeTable()}
                 />
                 {!collapsed && loading && <ActivityIndicator size="large" color="#0000ff" />}
-                {!collapsed && !loading && headers && <View style={{ width: 360 }}>
-                    <Header style={{ paddingLeft: 60, backgroundColor: 'white' }}>
+                {!collapsed && !loading && headers && <View style={{ marginLeft: -53 }}>
+                    <Header style={{ backgroundColor: 'white', paddingLeft: 66 }} textStyle={{ alignItems: 'center' }}>
                         {headers}
                     </Header>
                     {rows}
