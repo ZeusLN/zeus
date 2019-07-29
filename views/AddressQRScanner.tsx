b633a5ab0609565e55fb4e0429e106aa3ab48429
fiatjaf <fiatjaf@gmail.com>
2019-07-29 12:26:50 -0300

    invoke an external QR code reader.

--- views/AddressQRScanner.tsx
+++ views/AddressQRScanner.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Alert } from 'react-native';
 import { Icon } from 'react-native-elements';
 import AddressUtils from './../utils/AddressUtils';
+import ActivityResult from 'react-native-activity-result-fork';
 import QRCodeScanner from './../components/QRCodeScanner';
 import { inject, observer } from 'mobx-react';
 
@@ -15,6 +16,14 @@ interface AddressQRProps {
 @inject('NodeInfoStore')
 @observer
 export default class AddressQRScanner extends React.Component<AddressQRProps, {}> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            useInternalScanner: false
+        }
+    }
+
     handleAddressInvoiceScanned = (data: string) => {
         const { NodeInfoStore, navigation } = this.props;
         const { testnet } = NodeInfoStore;
@@ -49,8 +58,46 @@ export default class AddressQRScanner extends React.Component<AddressQRProps, {}
             navigation.navigate('Send');
         }
     }
+
+    async startExternalQRScanner () {
+        const { navigation } = this.props;
+
+        try {
+            const uniqueId = 0;
+            const response = await ActivityResult.startActivityForResult(uniqueId, 'com.google.zxing.client.android.SCAN', {'SCAN_MODE': 'QR_CODE_MODE'});
+
+            if (!response || response.resultCode === ActivityResult.CANCELED) {
+                // scan was canceled
+                navigation.goBack();
+                return;
+            }
+
+            if (response.resultCode !== ActivityResult.OK) {
+                // some different error, try internal scanner
+                this.setState({useInternalScanner: true});
+                return;
+            }
+
+            console.log(response.data.SCAN_RESULT);
+            this.handleAddressInvoiceScanned(response.data.SCAN_RESULT);
+        } catch (e) {
+            // it seems this will never happen
+            console.log(e);
+            this.setState({useInternalScanner: true});
+            return;
+        }
+    }
+
+    componentDidMount() {
+        this.startExternalQRScanner();
+    }
+
     render() {
         const { navigation } = this.props;
+
+        if (!this.state.useInternalScanner) {
+            return null;
+        }
 
         const BackButton = () => (
             <Icon
