1c5be7b7aa0fb73e351021a8359451dbe63e6e52
fiatjaf <fiatjaf@gmail.com>
2019-10-01 22:51:59 -0300

    external qr code reading fixes and improvements.

--- views/BTCPayConfigQRScanner.tsx
+++ views/BTCPayConfigQRScanner.tsx
@@ -1,62 +1,57 @@
-import * as React from 'react';
-import { Alert } from 'react-native';
-import { Icon } from 'react-native-elements';
-import QRCodeScanner from './../components/QRCodeScanner';
-import { inject, observer } from 'mobx-react';
+/** @format */
 
-import SettingsStore from './../stores/SettingsStore';
+import * as React from 'react'
+import {Alert} from 'react-native'
+import QRCodeScanner from './../components/QRCodeScanner'
+import {inject, observer} from 'mobx-react'
+
+import SettingsStore from './../stores/SettingsStore'
 
 interface BTCPayConfigQRProps {
-    navigation: any;
-    SettingsStore: SettingsStore;
+  navigation: any
+  SettingsStore: SettingsStore
 }
 
 @inject('SettingsStore')
 @observer
-export default class BTCPayConfigQRScanner extends React.Component<BTCPayConfigQRProps, {}> {
-    handleBTCPayConfigInvoiceScanned = (data: string) => {
-        const { SettingsStore, navigation } = this.props;
-        const { fetchBTCPayConfig } = SettingsStore;
+export default class BTCPayConfigQRScanner extends React.Component<
+  BTCPayConfigQRProps,
+  {}
+> {
+  handleBTCPayConfigInvoiceScanned = (data: string) => {
+    const {SettingsStore, navigation} = this.props
+    const {fetchBTCPayConfig} = SettingsStore
 
-        const index = navigation.getParam('index', null);
+    const index = navigation.getParam('index', null)
 
-        fetchBTCPayConfig(data).then((config: any) => {
-            navigation.navigate('AddEditNode', { node: config, index });
-        }).catch(() => {
-            Alert.alert(
-                'Error',
-                'Error fetching BTCPay config',
-                [
-                    {text: 'OK', onPress: () => void(0)}
-                ],
-                {cancelable: false}
-            );
+    fetchBTCPayConfig(data)
+      .then((config: any) => {
+        navigation.navigate('AddEditNode', {node: config, index})
+      })
+      .catch(() => {
+        Alert.alert(
+          'Error',
+          'Error fetching BTCPay config',
+          [{text: 'OK', onPress: () => void 0}],
+          {cancelable: false}
+        )
 
-            navigation.navigate('Settings');
-        });
-    }
+        navigation.navigate('Settings')
+      })
+  }
 
-    render() {
-        const { navigation } = this.props;
+  render() {
+    const {navigation} = this.props
 
-        const index = navigation.getParam('index', null);
+    const index = navigation.getParam('index', null)
 
-        const BackButton = () => (
-            <Icon
-                name="arrow-back"
-                onPress={() => navigation.navigate('AddEditNode', { index })}
-                color="#fff"
-                underlayColor="transparent"
-            />
-        );
-
-        return (
-            <QRCodeScanner
-                title="BTCPay Config QR Scanner"
-                text="Scan a BTCPay Config under Settings > Services > LND Rest"
-                handleQRScanned={this.handleBTCPayConfigInvoiceScanned}
-                BackButton={BackButton}
-            />
-        );
-    }
+    return (
+      <QRCodeScanner
+        title="BTCPay Config QR Scanner"
+        text="Scan a BTCPay Config under Settings > Services > LND Rest"
+        handleQRScanned={this.handleBTCPayConfigInvoiceScanned}
+        goBack={() => navigation.navigate('AddEditNode', {index})}
+      />
+    )
+  }
 }
