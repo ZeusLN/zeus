ef91be37afe382934108cb409b8683ae7b21af4e
Evan <evan@beatsperminute.com>
2019-10-19 13:45:20 -0400

    remove format tags

--- views/AddressQRScanner.tsx
+++ views/AddressQRScanner.tsx
@@ -1,5 +1,3 @@
-/** @format */
-
 import * as React from 'react';
 import { Alert } from 'react-native';
 import AddressUtils from './../utils/AddressUtils';
--- views/BTCPayConfigQRScanner.tsx
+++ views/BTCPayConfigQRScanner.tsx
@@ -1,5 +1,3 @@
-/** @format */
-
 import * as React from 'react';
 import { Alert } from 'react-native';
 import QRCodeScanner from './../components/QRCodeScanner';
