aee6c172af0bdef3905c5e1456a9ce2e364b232c
fiatjaf <fiatjaf@gmail.com>
2019-11-19 00:05:58 -0300

    lnurl-withdraw with js-lnurl.

--- stores/InvoicesStore.ts
+++ stores/InvoicesStore.ts
@@ -1,5 +1,6 @@
 import { action, observable } from 'mobx';
 import axios from 'axios';
+import { LNURLWithdrawParams } from 'js-lnurl';
 import Invoice from './../models/Invoice';
 import PaymentRequest from './../models/PaymentRequest';
 import SettingsStore from './SettingsStore';
@@ -82,7 +83,8 @@ export default class InvoicesStore {
     public createInvoice = (
         memo: string,
         value: string,
-        expiry: string = '3600'
+        expiry: string = '3600',
+        lnurlParams: LNURLWithdrawParams = undefined
     ) => {
         const { host, port, macaroonHex } = this.settingsStore;
 
@@ -109,6 +111,12 @@ export default class InvoicesStore {
                 const data = response.data;
                 this.payment_request = data.payment_request;
                 this.creatingInvoice = false;
+
+                if (lnurlParams) {
+                    axios.get(lnurlParams.callback, {
+                        params: { k1: lnurlParams.k1, pr: this.payment_request }
+                    });
+                }
             })
             .catch((error: any) => {
                 // handle error
