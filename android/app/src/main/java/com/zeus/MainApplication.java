65924bfc3f5bcabdf0d4e51c5c6399536266d94f
Evan <evan@beatsperminute.com>
2020-01-02 18:01:45 -0500

    init

--- android/app/src/main/java/com/zeus/MainApplication.java
+++ android/app/src/main/java/com/zeus/MainApplication.java
@@ -1,6 +1,11 @@
 package com.zeusln.zeus;
 
 import android.app.Application;
+import android.util.Log;
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.shell.MainReactPackage;
 
 import com.facebook.react.ReactApplication;
 import io.realm.react.RealmReactPackage;
@@ -12,41 +17,56 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+import android.content.Context;
+
 import com.facebook.soloader.SoLoader;
 import com.zeusln.zeus.extensions.CustomClientFactory;
 import com.microsoft.ActivityResultPackage;
 
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+    private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          packages.add(new MainReactPackage());
+          /*
+          packages.add(new RealmReactPackage());
+          packages.add(new LinearGradientPackage());
+          packages.add(new KeychainPackage());
+          packages.add(new RNCameraKitPackage());
+          packages.add(new RNGestureHandlerPackage());
+          packages.add(new VectorIconsPackage());
+          packages.add(new ActivityResultPackage());
+          */
+          /*
           new MainReactPackage(),
-            new RealmReactPackage(),
-            new LinearGradientPackage(),
-            new KeychainPackage(),
-            new RNCameraKitPackage(),
-            new RNGestureHandlerPackage(),
-            new VectorIconsPackage(),
-            new ActivityResultPackage()
-      );
-    }
+          new RealmReactPackage(),
+          new LinearGradientPackage(),
+          new KeychainPackage(),
+          new RNCameraKitPackage(),
+          new RNGestureHandlerPackage(),
+          new VectorIconsPackage(),
+          new ActivityResultPackage()
+          */
+          return packages;
+        }
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
