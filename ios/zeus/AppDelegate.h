ed2652732fe860bcf83e2e4278a4f89b89076d68
Evan <evan@beatsperminute.com>
2020-01-24 21:50:01 -0500

    Upgrade iOS app base

--- ios/zeus/AppDelegate.h
+++ ios/zeus/AppDelegate.h
@@ -6,9 +6,13 @@
  */
 
 #import <UIKit/UIKit.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
+#import <React/RCTBridgeDelegate.h>
+#import <UMCore/UMAppDelegateWrapper.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate>
 
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
 @property (nonatomic, strong) UIWindow *window;
 
 @end
