05b8fcf794d09a1ad44a50c642f97f25b3ae4afb
Evan <evan@beatsperminute.com>
2020-02-08 19:37:36 -0500

    fix iOS linking

--- ios/zeus/AppDelegate.m
+++ ios/zeus/AppDelegate.m
@@ -53,11 +53,11 @@
 #endif
 }
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-  return [RCTLinkingManager application:application openURL:url
-                      sourceApplication:sourceApplication annotation:annotation];
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
