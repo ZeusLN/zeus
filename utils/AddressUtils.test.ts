13865b7db6eefa7fae2bcd8ba42219fd77ec116a
fiatjaf <fiatjaf@alhur.es>
2020-09-16 22:44:00 -0300

    lndhub address matcher supports port numbers and paths.
    - the lnbits lndhub extension requires a path in the lndhub address
    - many lnbits personal installations are hosted in nonstandard port numbers
    - this also removes the strict lenghts of name and password fields

--- utils/AddressUtils.test.ts
+++ utils/AddressUtils.test.ts
@@ -190,14 +190,14 @@ describe('AddressUtils', () => {
                 ).toBeTruthy();
                 expect(
                     AddressUtils.isValidLNDHubAddress(
-                        'lndhub://123:9b9537fe3de0dc2a9c6b5b6475dd4047d5a4cf16e531fd4a3e37efb68c99b5d6@https://lntxbot.bigsun.xyz'
+                        'lndhub://admin:99b46a0892f7404e8a6e3a5e41d095cf@https://lnbits.local.domain.url:16507/lndhub/ext/'
                     )
                 ).toBeTruthy();
                 expect(
                     AddressUtils.isValidLNDHubAddress(
-                        'lndhub://9ae:9a1e4e972f732352c75e'
+                        'lndhub://123:9b9537fe3de0dc2a9c6b5b6475dd4047d5a4cf16e531fd4a3e37efb68c99b5d6@https://lntxbot.bigsun.xyz'
                     )
-                ).toBeFalsy();
+                ).toBeTruthy();
             });
         });
 
