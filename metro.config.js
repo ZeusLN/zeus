b7ddf0796e4cfa51254fcec013ee2a9d21db55b2
Evan Kaloudis <zeusln@tutanota.com>
2021-06-21 19:29:52 -0400

    Add react-native-svg-transformer

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer')
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  };
+})();
