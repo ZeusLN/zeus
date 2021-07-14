bd1954c77f46a8940dfad069e64d1f729f41674b
Evan Kaloudis <zeusln@tutanota.com>
2021-07-14 18:02:26 -0400

    more themeColor refactoring

--- views/ActivityFilter.tsx
+++ views/ActivityFilter.tsx
@@ -48,21 +48,7 @@ export default class ActivityFilter extends React.Component<
         workingEndDate: null
     };
 
-    renderSeparator = () => {
-        const { SettingsStore } = this.props;
-        const { settings } = SettingsStore;
-        const { theme } = settings;
-
-        return (
-            <View
-                style={
-                    theme === 'dark'
-                        ? styles.darkSeparator
-                        : styles.lightSeparator
-                }
-            />
-        );
-    };
+    renderSeparator = () => <View style={styles.separator} />;
 
     render() {
         const {
@@ -97,7 +83,7 @@ export default class ActivityFilter extends React.Component<
             endDate
         } = filters;
         const { settings } = SettingsStore;
-        const { theme, lurkerMode } = settings;
+        const { lurkerMode } = settings;
 
         const CloseButton = () => (
             <Icon
@@ -162,22 +148,12 @@ export default class ActivityFilter extends React.Component<
             <>
                 <>
                     {startDate && (
-                        <Text
-                            style={{
-                                color: theme === 'dark' ? 'white' : 'black',
-                                paddingTop: 30
-                            }}
-                        >{`${localeString(
+                        <Text style={styles.text}>{`${localeString(
                             'views.ActivityFilter.startDate'
                         )}: ${startDate.toString()}`}</Text>
                     )}
                     {endDate && (
-                        <Text
-                            style={{
-                                color: theme === 'dark' ? 'white' : 'black',
-                                paddingTop: 30
-                            }}
-                        >{`${localeString(
+                        <Text style={styles.text}>{`${localeString(
                             'views.ActivityFilter.endDate'
                         )}: ${endDate.toString()}`}</Text>
                     )}
@@ -278,13 +254,7 @@ export default class ActivityFilter extends React.Component<
         ];
 
         return (
-            <View
-                style={
-                    theme === 'dark'
-                        ? styles.darkThemeStyle
-                        : styles.lightThemeStyle
-                }
-            >
+            <View style={styles.view}>
                 <Header
                     leftComponent={<CloseButton />}
                     centerComponent={{
@@ -300,16 +270,12 @@ export default class ActivityFilter extends React.Component<
                             <ListItem
                                 containerStyle={{
                                     borderBottomWidth: 0,
-                                    backgroundColor:
-                                        theme === 'dark' ? '#1f2328' : 'white'
+                                    backgroundColor: themeColor('background')
                                 }}
                             >
                                 <ListItem.Title
                                     style={{
-                                        color:
-                                            theme === 'dark'
-                                                ? 'white'
-                                                : '#1f2328'
+                                        color: themeColor('text')
                                     }}
                                 >
                                     {item.label}
@@ -362,22 +328,18 @@ export default class ActivityFilter extends React.Component<
 }
 
 const styles = StyleSheet.create({
-    lightThemeStyle: {
+    view: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: themeColor('background'),
+        color: themeColor('text')
     },
-    darkThemeStyle: {
-        flex: 1,
-        backgroundColor: '#1f2328',
-        color: 'white'
+    text: {
+        color: themeColor('text'),
+        paddingTop: 30
     },
-    lightSeparator: {
+    separator: {
         height: 0.4,
-        backgroundColor: '#CED0CE'
-    },
-    darkSeparator: {
-        height: 0.4,
-        backgroundColor: 'darkgray'
+        backgroundColor: themeColor('separator')
     },
     button: {
         paddingTop: 15,
