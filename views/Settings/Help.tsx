4056437ed5e0cf1b591c23c90e9a1b9e262414c4
Evan Kaloudis <zeusln@tutanota.com>
2021-11-18 00:33:01 -0500

    Pill, NodeQRScanner, Help, Security to func components

--- views/Settings/Help.tsx
+++ views/Settings/Help.tsx
@@ -9,8 +9,10 @@ interface HelpProps {
     navigation: any;
 }
 
-export default class Help extends React.Component<HelpProps, {}> {
-    renderSeparator = () => (
+function Help(props: HelpProps) {
+    const { navigation } = props;
+
+    const renderSeparator = () => (
         <View
             style={{
                 height: 1,
@@ -19,72 +21,71 @@ export default class Help extends React.Component<HelpProps, {}> {
         />
     );
 
-    render() {
-        const { navigation } = this.props;
-        const BackButton = () => (
-            <Icon
-                name="arrow-back"
-                onPress={() => navigation.goBack()}
-                color={themeColor('text')}
-                underlayColor="transparent"
-            />
-        );
+    const BackButton = () => (
+        <Icon
+            name="arrow-back"
+            onPress={() => navigation.goBack()}
+            color={themeColor('text')}
+            underlayColor="transparent"
+        />
+    );
 
-        const HELP_ITEMS = [
-            { label: 'Telegram Group', url: 'https://t.me/ZeusLN' },
-            { label: 'Twitter (DMs open)', url: 'https://twitter.com/ZeusLN' },
-            {
-                label: 'GitHub Issues',
-                url: 'https://github.com/ZeusLN/zeus/issues'
-            }
-        ];
+    const HELP_ITEMS = [
+        { label: 'Telegram Group', url: 'https://t.me/ZeusLN' },
+        { label: 'Twitter (DMs open)', url: 'https://twitter.com/ZeusLN' },
+        {
+            label: 'GitHub Issues',
+            url: 'https://github.com/ZeusLN/zeus/issues'
+        }
+    ];
 
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    backgroundColor: themeColor('background')
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: themeColor('background')
+            }}
+        >
+            <Header
+                leftComponent={<BackButton />}
+                centerComponent={{
+                    text: localeString('general.help'),
+                    style: { color: themeColor('text') }
                 }}
-            >
-                <Header
-                    leftComponent={<BackButton />}
-                    centerComponent={{
-                        text: localeString('general.help'),
-                        style: { color: themeColor('text') }
-                    }}
-                    backgroundColor={themeColor('secondary')}
-                />
-                <FlatList
-                    data={HELP_ITEMS}
-                    renderItem={({ item, index }) => (
-                        <ListItem
-                            containerStyle={{
-                                borderBottomWidth: 0,
-                                backgroundColor: themeColor('background')
-                            }}
-                            onPress={() => UrlUtils.goToUrl(item.url)}
-                        >
-                            <ListItem.Content>
-                                <ListItem.Title
-                                    style={{
-                                        color: themeColor('text')
-                                    }}
-                                >
-                                    {item.label}
-                                </ListItem.Title>
-                            </ListItem.Content>
-                            <Text style={{ textAlign: 'right' }}>
-                                <Icon
-                                    name="keyboard-arrow-right"
-                                    color={themeColor('secondaryText')}
-                                />
-                            </Text>
-                        </ListItem>
-                    )}
-                    keyExtractor={(item, index) => `${item.label}-${index}`}
-                    ItemSeparatorComponent={this.renderSeparator}
-                />
-            </View>
-        );
-    }
+                backgroundColor={themeColor('secondary')}
+            />
+            <FlatList
+                data={HELP_ITEMS}
+                renderItem={({ item, index }) => (
+                    <ListItem
+                        containerStyle={{
+                            borderBottomWidth: 0,
+                            backgroundColor: themeColor('background')
+                        }}
+                        onPress={() => UrlUtils.goToUrl(item.url)}
+                    >
+                        <ListItem.Content>
+                            <ListItem.Title
+                                style={{
+                                    color: themeColor('text')
+                                }}
+                            >
+                                {item.label}
+                            </ListItem.Title>
+                        </ListItem.Content>
+                        <Text style={{ textAlign: 'right' }}>
+                            <Icon
+                                name="keyboard-arrow-right"
+                                color={themeColor('secondaryText')}
+                            />
+                        </Text>
+                    </ListItem>
+                )}
+                keyExtractor={(item, index) => `${item.label}-${index}`}
+                ItemSeparatorComponent={renderSeparator}
+            />
+        </View>
+    );
 }
+
+export default Help;
