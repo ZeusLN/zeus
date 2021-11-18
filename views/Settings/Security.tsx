d00592b9eb372dd36269f5e6d99629281a3544c3
Evan Kaloudis <zeusln@tutanota.com>
2021-11-18 00:33:01 -0500

    Pill, NodeQRScanner, Help, Security to func components

--- views/Settings/Security.tsx
+++ views/Settings/Security.tsx
@@ -9,8 +9,10 @@ interface SecurityProps {
     navigation: any;
 }
 
-export default class Security extends React.Component<SecurityProps, {}> {
-    renderSeparator = () => (
+function Security(props: SecurityProps) {
+    const { navigation } = props;
+
+    const renderSeparator = () => (
         <View
             style={{
                 height: 1,
@@ -19,71 +21,70 @@ export default class Security extends React.Component<SecurityProps, {}> {
         />
     );
 
-    render() {
-        const { navigation } = this.props;
-        const BackButton = () => (
-            <Icon
-                name="arrow-back"
-                onPress={() => navigation.goBack()}
-                color={themeColor('text')}
-                underlayColor="transparent"
-            />
-        );
+    const BackButton = () => (
+        <Icon
+            name="arrow-back"
+            onPress={() => navigation.goBack()}
+            color={themeColor('text')}
+            underlayColor="transparent"
+        />
+    );
 
-        const SECURITY_ITEMS = [
-            {
-                label: localeString('views.Settings.SetPassword.title'),
-                screen: 'SetPassword'
-            }
-            // { label: 'Verify TLS Certificate', url: 'https://twitter.com/ZeusLN' }
-        ];
+    const SECURITY_ITEMS = [
+        {
+            label: localeString('views.Settings.SetPassword.title'),
+            screen: 'SetPassword'
+        }
+        // { label: 'Verify TLS Certificate', url: 'https://twitter.com/ZeusLN' }
+    ];
 
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    backgroundColor: themeColor('background')
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: themeColor('background')
+            }}
+        >
+            <Header
+                leftComponent={<BackButton />}
+                centerComponent={{
+                    text: localeString('views.Settings.Security.title'),
+                    style: { color: themeColor('text') }
                 }}
-            >
-                <Header
-                    leftComponent={<BackButton />}
-                    centerComponent={{
-                        text: localeString('views.Settings.Security.title'),
-                        style: { color: themeColor('text') }
-                    }}
-                    backgroundColor={themeColor('secondary')}
-                />
-                <FlatList
-                    data={SECURITY_ITEMS}
-                    renderItem={({ item, index }) => (
-                        <ListItem
-                            containerStyle={{
-                                borderBottomWidth: 0,
-                                backgroundColor: themeColor('background')
-                            }}
-                            onPress={() => navigation.navigate(item.screen)}
-                        >
-                            <ListItem.Content>
-                                <ListItem.Title
-                                    style={{
-                                        color: themeColor('secondaryText')
-                                    }}
-                                >
-                                    {item.label}
-                                </ListItem.Title>
-                            </ListItem.Content>
-                            <Text style={{ textAlign: 'right' }}>
-                                <Icon
-                                    name="keyboard-arrow-right"
-                                    color={themeColor('secondaryText')}
-                                />
-                            </Text>
-                        </ListItem>
-                    )}
-                    keyExtractor={(item, index) => `${item.label}-${index}`}
-                    ItemSeparatorComponent={this.renderSeparator}
-                />
-            </View>
-        );
-    }
+                backgroundColor={themeColor('secondary')}
+            />
+            <FlatList
+                data={SECURITY_ITEMS}
+                renderItem={({ item, index }) => (
+                    <ListItem
+                        containerStyle={{
+                            borderBottomWidth: 0,
+                            backgroundColor: themeColor('background')
+                        }}
+                        onPress={() => navigation.navigate(item.screen)}
+                    >
+                        <ListItem.Content>
+                            <ListItem.Title
+                                style={{
+                                    color: themeColor('secondaryText')
+                                }}
+                            >
+                                {item.label}
+                            </ListItem.Title>
+                        </ListItem.Content>
+                        <Text style={{ textAlign: 'right' }}>
+                            <Icon
+                                name="keyboard-arrow-right"
+                                color={themeColor('secondaryText')}
+                            />
+                        </Text>
+                    </ListItem>
+                )}
+                keyExtractor={(item, index) => `${item.label}-${index}`}
+                ItemSeparatorComponent={renderSeparator}
+            />
+        </View>
+    );
 }
+
+export default Security;
