f96dabe55c112cb0a7813e8389f6fdec6db0dd5e
Evan Kaloudis <zeusln@tutanota.com>
2021-12-13 20:26:19 -0500

    Add node identicon to WalletHeader

--- components/WalletHeader.tsx
+++ components/WalletHeader.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { Button, Header } from 'react-native-elements';
-import { TouchableOpacity } from 'react-native';
+import { Avatar, Button, Header } from 'react-native-elements';
+import { Image, TouchableOpacity, View } from 'react-native';
+import Identicon from 'identicon.js';
 import RESTUtils from '../utils/RESTUtils';
+import PrivacyUtils from '../utils/PrivacyUtils';
 import { themeColor } from '../utils/ThemeUtils';
+import Keysign from '../images/SVG/Keysign.svg';
 import QRIcon from '../images/SVG/QR.svg';
 import { Body } from './text/Body';
 
-const SettingsButton = ({ navigation }: { navigation: any }) => (
-    <Button
-        title=""
-        icon={{
-            name: 'more-horiz',
-            size: 25,
-            color: themeColor('text')
-        }}
-        buttonStyle={{
-            backgroundColor: 'transparent',
-            marginRight: -10
-        }}
-        onPress={() => navigation.navigate('Settings')}
-    />
-);
+const hash = require('object-hash');
+const Head = require('../images/head.png');
 
 const OpenChannelButton = ({ navigation }: { navigation: any }) => (
     <Button
@@ -48,26 +38,78 @@ const QRBadge = ({ navigation }: { navigation: any }) => (
 
 export function WalletHeader({
     navigation,
+    SettingsStore,
     loading = false,
     title,
     channels = false
 }: {
     navigation: any;
+    SettingsStore: any;
     loading?: boolean;
     title?: string;
     channels?: boolean;
 }) {
+    const selectedNode: any =
+        (settings &&
+            settings.nodes &&
+            settings.nodes[settings.selectedNode || 0]) ||
+        null;
+
+    const displayName =
+        selectedNode && selectedNode.nickname
+            ? selectedNode.nickname
+            : selectedNode && selectedNode.implementation === 'lndhub'
+            ? selectedNode.lndhubUrl
+                  .replace('https://', '')
+                  .replace('http://', '')
+            : selectedNode && selectedNode.url
+            ? selectedNode.url.replace('https://', '').replace('http://', '')
+            : selectedNode && selectedNode.port
+            ? `${selectedNode.host}:${selectedNode.port}`
+            : (selectedNode && selectedNode.host) || 'Unknown';
+
+    const nodeTitle = PrivacyUtils.sensitiveValue(displayName, 8);
+    const implementation = PrivacyUtils.sensitiveValue(
+        (selectedNode && selectedNode.implementation) || 'lnd',
+        8
+    );
+
+    const data = new Identicon(
+        hash.sha1(
+            selectedNode && selectedNode.implementation === 'lndhub'
+                ? `${nodeTitle}-${selectedNode.username}`
+                : nodeTitle
+        ),
+        255
+    ).toString();
+
+    const SettingsButton = () => (
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+            {selectedNode ? (
+                <Avatar
+                    source={{
+                        uri: `data:image/png;base64,${data}`
+                    }}
+                    rounded
+                    size="medium"
+                    width={30}
+                    height={30}
+                />
+            ) : (
+                <Image source={Head} style={{ width: 30, height: 30 }} />
+            )}
+        </TouchableOpacity>
+    );
+
     return (
         <Header
-            leftComponent={
-                loading ? undefined : <QRBadge navigation={navigation} />
-            }
+            leftComponent={loading ? undefined : <SettingsButton />}
             centerComponent={title ? <Body bold>{title}</Body> : undefined}
             rightComponent={
                 channels ? (
                     <OpenChannelButton navigation={navigation} />
                 ) : (
-                    <SettingsButton navigation={navigation} />
+                    <QRBadge navigation={navigation} />
                 )
             }
             backgroundColor="transparent"
