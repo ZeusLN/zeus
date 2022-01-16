e63fe2b60e3631f1d6327ccdeb179de2bdb68d10
Evan Kaloudis <zeusln@tutanota.com>
2022-01-16 15:37:51 -0500

    Fix import order

--- views/Routing/Routing.tsx
+++ views/Routing/Routing.tsx
@@ -3,11 +3,10 @@ import { FlatList, View, Text, TouchableOpacity } from 'react-native';
 import { ButtonGroup, Header, Icon } from 'react-native-elements';
 import { inject, observer } from 'mobx-react';
 
-import { RoutingListItem } from './RoutingListItem';
-import { RoutingHeader } from './RoutingHeader';
-
 import { Spacer } from '../../components/layout/Spacer';
 import LoadingIndicator from '../../components/LoadingIndicator';
+
+import Pie from '../../images/SVG/Pie.svg';
 
 import FeeStore from '../../stores/FeeStore';
 import SettingsStore from '../../stores/SettingsStore';
@@ -15,7 +14,8 @@ import SettingsStore from '../../stores/SettingsStore';
 import { localeString } from '../../utils/LocaleUtils';
 import { themeColor } from './../../utils/ThemeUtils';
 
-import Pie from '../../images/SVG/Pie.svg';
+import { RoutingListItem } from './RoutingListItem';
+import { RoutingHeader } from './RoutingHeader';
 
 interface RoutingProps {
     navigation: any;
