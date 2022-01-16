4cf48e9f337ab25edaf6c93d4478815b2f83efb3
Evan Kaloudis <zeusln@tutanota.com>
2022-01-15 21:26:46 -0500

    Add Scan icon to wallet header

--- components/WalletHeader.tsx
+++ components/WalletHeader.tsx
@@ -7,7 +7,7 @@ import RESTUtils from '../utils/RESTUtils';
 import PrivacyUtils from '../utils/PrivacyUtils';
 import { themeColor } from '../utils/ThemeUtils';
 import Keysign from '../images/SVG/Keysign.svg';
-import QRIcon from '../images/SVG/QR.svg';
+import Scan from '../images/SVG/Scan.svg';
 import { Body } from './text/Body';
 
 const OpenChannelButton = ({ navigation }: { navigation: any }) => (
@@ -26,11 +26,11 @@ const OpenChannelButton = ({ navigation }: { navigation: any }) => (
     />
 );
 
-const QRBadge = ({ navigation }: { navigation: any }) => (
+const ScanBadge = ({ navigation }: { navigation: any }) => (
     <TouchableOpacity
         onPress={() => navigation.navigate('AddressQRCodeScanner')}
     >
-        <QRIcon fill={themeColor('text')} />
+        <Scan fill={themeColor('text')} />
     </TouchableOpacity>
 );
 
@@ -71,7 +71,7 @@ export function WalletHeader({
                 channels ? (
                     <OpenChannelButton navigation={navigation} />
                 ) : (
-                    <QRBadge navigation={navigation} />
+                    <ScanBadge navigation={navigation} />
                 )
             }
             backgroundColor="transparent"
