45897f1c5369ec3e69fb7bbcd0f8fa6fca5a4e91
Evan Kaloudis <zeusln@tutanota.com>
2022-02-05 15:13:50 -0500

    Remove channels donuts

--- components/Channels/ChannelsHeader.tsx
+++ components/Channels/ChannelsHeader.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Svg, { G, Circle } from 'react-native-svg';
 import { themeColor } from '../../utils/ThemeUtils';
 
 import { Body } from '../text/Body';
@@ -12,86 +11,6 @@ import { Amount } from '../Amount';
 const outboundYellow = themeColor('outbound');
 const inboundYellow = themeColor('inbound');
 const offlineGray = themeColor('secondaryText');
-
-export function ChannelsDonut(props) {
-    const { totalInbound, totalOutbound, totalOffline, radius } = props;
-
-    const strokeWidth = 6;
-
-    const halfCircle = radius + strokeWidth;
-    const circumference = 2 * Math.PI * radius;
-    const color = 'black';
-
-    const total = totalInbound + totalOutbound + totalOffline;
-    const outboundLength = (totalOutbound / total) * circumference;
-    const inboundLength = (totalInbound / total) * circumference;
-    const offlineLength = (totalOffline / total) * circumference;
-
-    const height = radius * 2;
-
-    return (
-        <View style={{ width: height }}>
-            <Svg
-                height={height}
-                width={height}
-                viewBox={`0 0 ${halfCircle * 2} ${halfCircle * 2}`}
-            >
-                <G rotation="-90" origin={`${halfCircle}, ${halfCircle}`}>
-                    {/* Outbound */}
-                    <Circle
-                        cx="50%"
-                        cy="50%"
-                        r={radius}
-                        fill="transparent"
-                        stroke={outboundYellow}
-                        strokeWidth={strokeWidth}
-                        strokeDashoffset={0}
-                        strokeDasharray={`${outboundLength}, ${
-                            circumference - outboundLength
-                        }`}
-                    />
-                    {/* Inbound */}
-                    <Circle
-                        cx="50%"
-                        cy="50%"
-                        r={radius}
-                        fill="transparent"
-                        stroke={inboundYellow}
-                        strokeWidth={strokeWidth}
-                        strokeDashoffset={-outboundLength}
-                        strokeDasharray={`${inboundLength}, ${
-                            circumference - inboundLength
-                        }`}
-                    />
-                    {/* Offline */}
-                    <Circle
-                        cx="50%"
-                        cy="50%"
-                        r={radius}
-                        fill="transparent"
-                        stroke={offlineGray}
-                        strokeWidth={strokeWidth}
-                        strokeDashoffset={-(outboundLength + inboundLength)}
-                        strokeDasharray={`${offlineLength}, ${
-                            circumference - offlineLength
-                        }`}
-                    />
-                    {/* Hidden circle behind in case we're bad at math */}
-                    <Circle
-                        cx="50%"
-                        cy="50%"
-                        r={radius}
-                        fill="transparent"
-                        stroke={color}
-                        strokeWidth={strokeWidth}
-                        strokeLinejoin="round"
-                        strokeOpacity=".1"
-                    />
-                </G>
-            </Svg>
-        </View>
-    );
-}
 
 function TotalRow({
     kind,
@@ -153,12 +72,6 @@ export function ChannelsHeader(props) {
 }
 
 const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 16
-    },
     wrapper: {
         display: 'flex',
         justifyContent: 'space-between',
