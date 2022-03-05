41df2ed552d69cc76926c96e53f3bed53f526e45
Evan Kaloudis <zeusln@tutanota.com>
2022-03-04 19:55:43 -0500

    Add Become a Community Sponsor button

--- views/Settings/Olympians.tsx
+++ views/Settings/Olympians.tsx
@@ -3,6 +3,7 @@ import { FlatList, Text, View } from 'react-native';
 import { Avatar, Header, Icon, ListItem } from 'react-native-elements';
 import { inject, observer } from 'mobx-react';
 
+import Button from './../../components/Button';
 import LoadingIndicator from './../../components/LoadingIndicator';
 
 import { localeString } from './../../utils/LocaleUtils';
@@ -59,35 +60,49 @@ export default class Olympians extends React.Component<OlympiansProps, {}> {
                 />
                 {loading && <LoadingIndicator />}
                 {!loading && !olympiansError && (
-                    <FlatList
-                        data={olympians.slice().reverse()}
-                        renderItem={({ item }) => (
-                            <ListItem
-                                containerStyle={{
-                                    backgroundColor: themeColor('background'),
-                                    padding: 0
-                                }}
-                                onPress={() =>
-                                    UrlUtils.goToUrl(
-                                        `https://twitter.com/${item.handle}`
-                                    )
-                                }
-                            >
-                                <Avatar
-                                    size={76}
-                                    rounded
-                                    source={{
-                                        uri: `https://zeusln.app/api/twitter-images/${item.handle}.jpg`
+                    <>
+                        <FlatList
+                            data={olympians.slice().reverse()}
+                            renderItem={({ item }) => (
+                                <ListItem
+                                    containerStyle={{
+                                        backgroundColor:
+                                            themeColor('background'),
+                                        padding: 0
                                     }}
-                                    key={1}
-                                    containerStyle={{ margin: 10 }}
-                                />
-                            </ListItem>
-                        )}
-                        numColumns={4}
-                        keyExtractor={(item, index) => index}
-                        style={{ alignSelf: 'center' }}
-                    />
+                                    onPress={() =>
+                                        UrlUtils.goToUrl(
+                                            `https://twitter.com/${item.handle}`
+                                        )
+                                    }
+                                >
+                                    <Avatar
+                                        size={76}
+                                        rounded
+                                        source={{
+                                            uri: `https://zeusln.app/api/twitter-images/${item.handle}.jpg`
+                                        }}
+                                        key={1}
+                                        containerStyle={{ margin: 10 }}
+                                    />
+                                </ListItem>
+                            )}
+                            numColumns={4}
+                            keyExtractor={(item, index) => index}
+                            style={{ alignSelf: 'center' }}
+                        />
+                        <Button
+                            title={localeString(
+                                'views.Olympians.becomeASponsor'
+                            )}
+                            containerStyle={{
+                                margin: 10
+                            }}
+                            onPress={() =>
+                                UrlUtils.goToUrl('https://zeusln.app/about')
+                            }
+                        />
+                    </>
                 )}
                 {olympiansError && (
                     <Text
