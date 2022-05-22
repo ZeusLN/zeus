340cb84be851a5376ac25b774b22d4e62623cce1
Evan Kaloudis <zeusln@tutanota.com>
2022-05-22 00:59:27 -0400

    ErrorUtils: user friendly error messages

---
+++ utils/ErrorUtils.ts
@@ -0,0 +1,15 @@
+const userFriendlyErrors: any = {
+    'Error: SOCKS: Connection refused':
+        'Host unreachable. Try restarting your node or its Tor process.',
+    'Error: SOCKS: Host unreachable':
+        'Host unreachable. Try restarting your node or its Tor process.',
+    'Error: called `Result::unwrap()` on an `Err` value: BootStrapError("Timeout waiting for bootstrap")':
+        'Error starting up Tor on your phone. Try restarting Zeus. If the problem persists consider reinstalling the app.'
+};
+
+class ErrorUtils {
+    errorToUserFriendly = (error: string) => userFriendlyErrors[error] || error;
+}
+
+const errorUtils = new ErrorUtils();
+export default errorUtils;
