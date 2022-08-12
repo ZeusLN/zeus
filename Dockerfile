d6cf6a11790d84c80463a51ef5590bdbaf714b1b
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-08-12 19:05:50 +0200

    Simplifiying process

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM reactnativecommunity/react-native-android:latest
-ARG tag_name=master
 
 WORKDIR /olympus
-
-RUN git clone --depth 1 --branch ${tag_name} https://github.com/ZeusLN/zeus.git
-WORKDIR /olympus/zeus
-RUN npm install
 
 CMD /bin/bash
--- build.sh
+++ build.sh
@@ -1,45 +1,16 @@
 #!/bin/bash
 set -x
-BUILD_TAG=$1
 IMAGE_NAME="zeus_builder_image"
 CONTAINER_NAME="zeus_builder_container"
-PASS_FILE="android/app/olympus_keystore.pass"
-KEYSTORE_FILE="android/app/olympus.pfx"
-ALIAS="olympus"
-
 ZEUS_PATH=/olympus/zeus
 
-# Check we have the password.
-if [ -f $PASS_FILE ]; then
-    KEYSTORE_PASS=$(cat magen_keystore.pass);
-else
-    KEYSTORE_PASS=$(head -c 32 /dev/urandom | sha256sum | head -c 64);
-    echo $KEYSTORE_PASS > $PASS_FILE;
-fi
-
-# Check we have the keystore.
-if [ ! -f $KEYSTORE_FILE ]; then
-#    keytool -genkey -alias $ALIAS -keystore $KEYSTORE_FILE -storetype PKCS12 -keyalg RSA -keysize 4096 -storepass $KEYSTORE_PASS -keypass $KEYSTORE_PASS -validity 10000 -dname CN=IL;
-    keytool -genkeypair -alias $ALIAS -keystore $KEYSTORE_FILE -v -storetype PKCS12 -keyalg RSA -keysize 2048 -storepass $KEYSTORE_PASS -keypass $KEYSTORE_PASS -validity 10000
-fi
-
-ZEUS_KEY_PASSWORD=$ZEUS_STORE_PASSWORD
-
-KEYSTORE_FILE="$ZEUS_PATH/$KEYSTORE_FILE"
-
-docker build --build-arg tag_name=${BUILD_TAG} . -t $IMAGE_NAME
+docker build -t $IMAGE_NAME .
 
 docker run --name $CONTAINER_NAME -v `pwd`:$ZEUS_PATH $IMAGE_NAME bash -c \
-    "cd /olympus/zeus/android && \
-    ZEUS_KEYSTORE_PATH=$KEYSTORE_FILE \
-    ZEUS_KEY_PASSWORD=$KEYSTORE_PASS \
-    ZEUS_STORE_PASSWORD=$KEYSTORE_PASS \
-    ZEUS_KEY_ALIAS=$ALIAS \
-    ZEUS_DEBUG_KEYSTORE_PATH=dummy \
-    ZEUS_DEBUG_STORE_PASSWORD=dummy \
-    ZEUS_DEBUG_KEY_ALIAS=dummy \
-    ZEUS_DEBUG_KEY_PASSWORD=dummy \
-    ./gradlew --debug assembleDebug && \
-    cp ./app/build/outputs/apk/debug/zeus-debug.apk ../";
+     "cd /olympus/zeus && \
+      npm install && \
+      cd /olympus/zeus/android && \
+      ./gradlew --debug assembleRelease && \
+      cp ./app/build/outputs/apk/debug/zeus-release.apk ../";
 
 docker container rm $CONTAINER_NAME
