f4029cf76777b0234b0806f5fc91b4061adcf754
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-08-14 12:15:46 +0200

    Simplify / beautify build script

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ CONTAINER_NAME="zeus_builder_container"
 ZEUS_PATH=/olympus/zeus
 
 docker run --rm --name $CONTAINER_NAME -v `pwd`:$ZEUS_PATH $BUILDER_IMAGE bash -c \
-     "cd /olympus/zeus && \
-      yarn install && \
-      cd /olympus/zeus/android && \
-      ./gradlew assembleRelease && \
+     "cd /olympus/zeus ; yarn install && \
+      cd /olympus/zeus/android ; ./gradlew assembleRelease && \
       cd /olympus/zeus ; find android/app/build/outputs/apk/release | grep app-";
