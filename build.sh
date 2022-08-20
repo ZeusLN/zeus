6a453f4597cbc6044a840052773281bf697ebb26
Luis Miguel García Mancebo <luismiguel@leanxcale.com>
2022-08-20 17:30:37 +0200

    Delete unused variables

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@ BUILDER_IMAGE="reactnativecommunity/react-native-android:latest"
 CONTAINER_NAME="zeus_builder_container"
 ZEUS_PATH=/olympus/zeus
 
-KEYSTORE_FILE=zeus.keystore
-KEYSTORE_PASS=zeus1234
-ALIAS=zeus
-
 docker run --rm --name $CONTAINER_NAME -v `pwd`:$ZEUS_PATH $BUILDER_IMAGE bash -c \
      'echo -e "\n\n********************************\n*** Building Zeus...\n********************************\n" && \
       cd /olympus/zeus ; yarn install && \
