15ec63aec32fc8cee7eea0d1f0359aeb29709cba
Luis Miguel <ktecho@ktecho.com>
2022-08-24 15:32:25 +0200

    Reproducible builds (#1055)
    * Simplre reproducible builds
    
    * clone the branch you're reproducing
    
    * fix branch/tag argument taking + use master as default
    
    * be able to execute build.sh more than once
    
    * Simplifiying process
    
    * Updating rn-nodeify to 10.3.0 to avoid issue
    
    * Deleting compile and Dockerfile as they're not used anymore
    
    * Updated simplified build script and README file with instructions to build
    
    * Simplify / beautify build script
    
    * Commenting out the singning process for release in build.gradle
    
    * Build and sign every apk
    
    * Cut apk location up to current location for easy location of files
    
    * Remove signing code
    
    * Delete unused variables
    
    * Updated instructions
    
    * README: update reproducible instructions
    
    * build.sh: lock in Docker image version
    
    reactnativecommunity/react-native-android:5.4
    
    Co-authored-by: Evan Kaloudis <kaloudis@users.noreply.github.com>

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+BUILDER_IMAGE="reactnativecommunity/react-native-android:5.4"
+CONTAINER_NAME="zeus_builder_container"
+ZEUS_PATH=/olympus/zeus
+
+docker run --rm --name $CONTAINER_NAME -v `pwd`:$ZEUS_PATH $BUILDER_IMAGE bash -c \
+     'echo -e "\n\n********************************\n*** Building Zeus...\n********************************\n" && \
+      cd /olympus/zeus ; yarn install && \
+      cd /olympus/zeus/android ; ./gradlew assembleRelease && \
+
+      echo -e "\n\n********************************\n**** APKs and MD5\n********************************\n" && \
+      cd /olympus/zeus && \
+      for f in android/app/build/outputs/apk/release/*.apk;
+      do
+	      RENAMED_FILENAME=$(echo $f | sed -e "s/app-/zeus-/" | sed -e "s/-release-unsigned//")
+	      mv $f $RENAMED_FILENAME
+	      md5sum $RENAMED_FILENAME
+      done && \
+      echo -e "\n" ';
+
