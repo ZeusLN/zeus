f3ca19186201d17fd866998e2cae228f4955267e
Michael Little <michael@wrtdesign.net>
2022-11-23 11:56:55 +1100

    added localization for user friendly error messages

--- utils/ErrorUtils.ts
+++ utils/ErrorUtils.ts
@@ -1,16 +1,22 @@
 const userFriendlyErrors: any = {
-    'Error: SOCKS: Connection refused':
-        'Host unreachable. Try restarting your node or its Tor process.',
-    'Error: SOCKS: Host unreachable':
-        'Host unreachable. Try restarting your node or its Tor process.',
+    'Error: SOCKS: Connection refused': 'error.connectionRefused',
+    'Error: SOCKS: Host unreachable': 'error.hostUnreachable',
     'Error: called `Result::unwrap()` on an `Err` value: BootStrapError("Timeout waiting for bootstrap")':
-        'Error starting up Tor on your phone. Try restarting Zeus. If the problem persists consider reinstalling the app.',
+        'error.torBootstrap',
     'Error: called `Result::unwrap()` on an `Err` value: BootStrapError("Timeout waiting for boostrap")':
-        'Error starting up Tor on your phone. Try restarting Zeus. If the problem persists consider reinstalling the app.'
+        'error.torBootstrap'
 };
 
 class ErrorUtils {
-    errorToUserFriendly = (error: string) => userFriendlyErrors[error] || error;
+    errorToUserFriendly = (error: string, localize = true) => {
+        if (localize) {
+            const localeString = require('./LocaleUtils').localeString;
+            return localeString(userFriendlyErrors[error]) || error;
+        } else {
+            const EN = require('../locales/en.json');
+            return EN[userFriendlyErrors[error]] || error;
+        }
+    };
 }
 
 const errorUtils = new ErrorUtils();
