0bf35fdbb3bb3bfae9669578fb2d35fd3f258e7c
Evan Kaloudis <kaloudis@users.noreply.github.com>
2022-12-13 00:55:43 -0500

    SettingsStore: INTERFACE_KEYS: Spark -> Sparko
    Define Spark interface as Sparko (for now, until updates are committed)

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -58,7 +58,7 @@ export const INTERFACE_KEYS = [
     { key: 'LND (REST)', value: 'lnd' },
     { key: 'LND (Lightning Node Connect)', value: 'lightning-node-connect' },
     { key: 'Core Lightning (c-lightning-REST)', value: 'c-lightning-REST' },
-    { key: 'Core Lightning (Spark) [Experimental]', value: 'spark' },
+    { key: 'Core Lightning (Sparko)', value: 'spark' },
     { key: 'Eclair', value: 'eclair' },
     { key: 'LNDHub', value: 'lndhub' }
 ];
