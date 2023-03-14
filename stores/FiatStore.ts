49483f4ea2bf503d8ca9de2ecbcb5b4c58b9a7ab
Evan Kaloudis <zeusln@tutanota.com>
2023-03-13 20:16:05 -0400

    Fiat currencies: restore ISK + add LBP, MYR, UAH, JMD, COP, MXN

--- stores/FiatStore.ts
+++ stores/FiatStore.ts
@@ -28,6 +28,8 @@ export default class FiatStore {
             y === ',' ? '.' : ','
         );
 
+    // Resource below may be helpful for formatting
+    // https://fastspring.com/blog/how-to-format-30-currencies-from-countries-all-over-the-world/
     private symbolLookup = (symbol: string): CurrencyDisplayRules => {
         const symbolPairs: any = {
             USD: {
@@ -53,6 +55,7 @@ export default class FiatStore {
             },
             CLP: { symbol: '$', space: true, rtl: false, separatorSwap: false },
             CNY: { symbol: '¥', space: true, rtl: false, separatorSwap: false },
+            COP: { symbol: '$', space: true, rtl: false, separatorSwap: false },
             CZK: {
                 symbol: 'Kč',
                 space: true,
@@ -91,8 +94,28 @@ export default class FiatStore {
             },
             ILS: { symbol: '₪', space: true, rtl: false, separatorSwap: false },
             INR: { symbol: '₹', space: true, rtl: false, separatorSwap: false },
+            ISK: { symbol: 'kr', space: true, rtl: true, separatorSwap: true },
+            JMD: {
+                symbol: '$',
+                space: false,
+                rtl: false,
+                separatorSwap: false
+            },
             JPY: { symbol: '¥', space: true, rtl: false, separatorSwap: false },
             KRW: { symbol: '₩', space: true, rtl: false, separatorSwap: false },
+            LBP: {
+                symbol: 'LL',
+                space: true,
+                rtl: false,
+                separatorSwap: false
+            },
+            MXN: { symbol: '$', space: true, rtl: false, separatorSwap: false },
+            MYR: {
+                symbol: 'MR',
+                space: true,
+                rtl: false,
+                separatorSwap: false
+            },
             NGN: {
                 symbol: '₦',
                 space: false,
@@ -122,7 +145,13 @@ export default class FiatStore {
             },
             THB: { symbol: '฿', space: true, rtl: true, separatorSwap: false },
             TRY: { symbol: '₺', space: true, rtl: true, separatorSwap: false },
-            TWD: { symbol: '元', space: true, rtl: false, separatorSwap: false }
+            TWD: {
+                symbol: '元',
+                space: true,
+                rtl: false,
+                separatorSwap: false
+            },
+            UAH: { symbol: '₴', space: false, rtl: false, separatorSwap: true }
         };
 
         if (symbol in symbolPairs) {
@@ -185,9 +214,45 @@ export default class FiatStore {
         return '$N/A';
     };
 
+    // as of March 13, 2023
     // BTCPAY rates string:
-    // BTC_USD,BTC_AUD,BTC_BRL,BTC_CAD,BTC_CHF,BTC_CLP,BTC_CNY,BTC_CZK,BTC_DKK,BTC_EUR,BTC_GBP,BTC_HKD,BTC_HRK,BTC_HUF,BTC_INR,BTC_ISK,BTC_JPY,BTC_KRW,BTC_NZD,BTC_PLN,BTC_RON,BTC_RUB,BTC_SEK,BTC_SGD,BTC_THB,BTC_TRY,BTC_TWD,BTC_ILS,BTC_ARS,BTC_NGN
-    // rates via CoinGecko
+    // BTC_USD,BTC_AUD,BTC_BRL,BTC_CAD,BTC_CHF,BTC_CLP,BTC_CNY,BTC_CZK,BTC_DKK,BTC_EUR,BTC_GBP,BTC_HKD,BTC_HUF,BTC_INR,BTC_ISK,BTC_JPY,BTC_KRW,BTC_NZD,BTC_PLN,BTC_RON,BTC_RUB,BTC_SEK,BTC_SGD,BTC_THB,BTC_TRY,BTC_TWD,BTC_ILS,BTC_ARS,BTC_NGN,BTC_LBP,BTC_MYR,BTC_UAH,BTC_JMD,BTC_COP,BTC_MXN
+    // BTCPAY custom scripting :
+    // BTC_USD = coingecko(BTC_USD);
+    // BTC_AUD = coingecko(BTC_AUD);
+    // BTC_BRL = coingecko(BTC_BRL);
+    // BTC_CAD = coingecko(BTC_CAD);
+    // BTC_CHF = coingecko(BTC_CHF);
+    // BTC_CLP = coingecko(BTC_CLP);
+    // BTC_CNY = coingecko(BTC_CNY);
+    // BTC_CZK = coingecko(BTC_CZK);
+    // BTC_DKK = coingecko(BTC_DKK);
+    // BTC_EUR = coingecko(BTC_EUR);
+    // BTC_GBP = coingecko(BTC_GBP);
+    // BTC_HKD = coingecko(BTC_HKD);
+    // BTC_HUF = coingecko(BTC_HUF);
+    // BTC_INR = coingecko(BTC_INR);
+    // BTC_ISK = bitpay(BTC_ISK);
+    // BTC_JPY = coingecko(BTC_JPY);
+    // BTC_KRW = coingecko(BTC_KRW);
+    // BTC_NZD = coingecko(BTC_NZD);
+    // BTC_PLN = coingecko(BTC_PLN);
+    // BTC_RON = yadio(BTC_RON);
+    // BTC_RUB = coingecko(BTC_RUB);
+    // BTC_SEK = coingecko(BTC_SEK);
+    // BTC_SGD = coingecko(BTC_SGD);
+    // BTC_THB = coingecko(BTC_THB);
+    // BTC_TRY = coingecko(BTC_TRY);
+    // BTC_TWD = coingecko(BTC_TWD);
+    // BTC_ILS = coingecko(BTC_ILS);
+    // BTC_ARS = coingecko(BTC_ARS);
+    // BTC_NGN = coingecko(BTC_NGN);
+    // BTC_LBP = yadio(BTC_LBP);
+    // BTC_MYR = yadio(BTC_MYR);
+    // BTC_UAH = yadio(BTC_UAH);
+    // BTC_JMD = yadio(BTC_JMD);
+    // BTC_COP = yadio(BTC_COP);
+    // BTC_MXN = yadio(BTC_MXN);
     @action
     public getFiatRates = () => {
         this.loading = true;
