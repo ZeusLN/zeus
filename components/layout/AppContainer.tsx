07865599bb76e80625da35aeb68d705dc3e0516a
Evan Kaloudis <zeusln@tutanota.com>
2023-03-20 12:06:23 -0400

    layout/AppContainer: remove SafeAreaView for Screen gradient on iOS

--- components/layout/AppContainer.tsx
+++ components/layout/AppContainer.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
 import { Observer } from 'mobx-react';
 
 import Screen from '../../components/Screen';
 
-export function AppContainer({
-    style = {},
-    children
-}: {
-    style?: any;
-    children?: React.ReactNode;
-}) {
+export function AppContainer({ children }: { children?: React.ReactNode }) {
     return (
         <Observer>
             {() => {
-                return (
-                    <Screen>
-                        <SafeAreaView
-                            style={{
-                                flex: 1,
-                                ...style
-                            }}
-                        >
-                            {children}
-                        </SafeAreaView>
-                    </Screen>
-                );
+                return <Screen>{children}</Screen>;
             }}
         </Observer>
     );
