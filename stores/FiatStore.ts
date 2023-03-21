9b2568bb056bcff0d14c941aa995fdfb3671c33d
Evan Kaloudis <zeusln@tutanota.com>
2023-03-20 22:07:29 -0400

    FiatStore: getFiatRates: alleviate load on endpoint

--- stores/FiatStore.ts
+++ stores/FiatStore.ts
@@ -394,6 +394,8 @@ export default class FiatStore {
     // BTC_DOP = yadio(BTC_DOP);
     @action
     public getFiatRates = () => {
+        // try not to slam endpoint
+        if (this.loading) return;
         this.loading = true;
         ReactNativeBlobUtil.fetch(
             'GET',
