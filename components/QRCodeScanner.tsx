bfcf3f0be65261e679518b9e2761a656e19aa0c3
Shubham Kumar <shubham.kumar@uniacco.com>
2023-04-04 23:32:03 +0530

    Added torch functionality to scan QR code

--- components/QRCodeScanner.tsx
+++ components/QRCodeScanner.tsx
@@ -9,7 +9,6 @@ import {
 } from 'react-native';
 import { CameraScreen, Camera } from 'react-native-camera-kit';
 import { launchImageLibrary } from 'react-native-image-picker';
-// import Permissions, { PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 const LocalQRCode = require('@remobile/react-native-qrcode-local-image');
 
@@ -31,10 +30,8 @@ interface QRProps {
 }
 
 interface QRState {
-    cameraAuthorized: any;
-    isCameraOpen: boolean;
-    qrCodeData: string;
-    flashMode: any;
+    cameraStatus: any;
+    isTorchOn: boolean;
 }
 
 export default class QRCodeScanner extends React.Component<QRProps, QRState> {
@@ -42,19 +39,18 @@ export default class QRCodeScanner extends React.Component<QRProps, QRState> {
         super(props);
 
         this.state = {
-            cameraAuthorized: false,
-            isCameraOpen: false,
-            qrCodeData: '',
-            flashMode: 'off'
+            cameraStatus: '',
+            isTorchOn: false
         };
     }
     scannedCache: { [name: string]: number } = {};
     maskLength = (Dimensions.get('window').width * 80) / 100;
 
-    toggleFlash = () => {
-        const { flashMode } = this.state;
-        this.setState({ flashMode: flashMode === 'torch' ? 'off' : 'torch' });
-    };
+    CameraAuthStatus = Object.freeze({
+        AUTHORIZED: 'AUTHORIZED',
+        NOT_AUTHORIZED: 'NOT_AUTHORIZED',
+        UNKNOWN: 'UNKNOWN'
+    });
 
     handleRead = (data: any) => {
         const hash = createHash('sha256').update(data).digest().toString('hex');
@@ -91,60 +87,150 @@ export default class QRCodeScanner extends React.Component<QRProps, QRState> {
         this.handleRead(event.nativeEvent.codeStringValue);
     };
 
+    toggleTorch = async () => {
+        const { isTorchOn } = this.state;
+        try {
+            this.setState({ isTorchOn: !isTorchOn });
+        } catch (error) {
+            console.log('Error toggling torch: ', error);
+        }
+    };
+
+    async componentDidMount() {
+        if (Platform.OS !== 'ios' && Platform.OS !== 'macos') {
+            return;
+        }
+        let isUserAuthorizedCamera;
+        const isCameraAuthorized =
+            await Camera.checkDeviceCameraAuthorizationStatus();
+        switch (isCameraAuthorized) {
+            case true:
+                this.setState({
+                    cameraStatus: this.CameraAuthStatus.AUTHORIZED
+                });
+
+                break;
+            case false:
+                this.setState({
+                    cameraStatus: this.CameraAuthStatus.NOT_AUTHORIZED
+                });
+                isUserAuthorizedCamera =
+                    await Camera.requestDeviceCameraAuthorization();
+                if (isUserAuthorizedCamera) {
+                    this.setState({
+                        cameraStatus: this.CameraAuthStatus.NOT_AUTHORIZED
+                    });
+                }
+                break;
+            case -1:
+                this.setState({
+                    cameraStatus: this.CameraAuthStatus.UNKNOWN
+                });
+                isUserAuthorizedCamera =
+                    await Camera.requestDeviceCameraAuthorization();
+                if (isUserAuthorizedCamera) {
+                    this.setState({
+                        cameraStatus: this.CameraAuthStatus.AUTHORIZED
+                    });
+                }
+                break;
+        }
+    }
+
     render() {
+        const { cameraStatus, isTorchOn } = this.state;
         const { text, goBack } = this.props;
 
         return (
-            <View style={styles.container}>
-                <CameraScreen
-                    scanBarcode={true}
-                    onReadCode={this.onQRCodeScan}
-                    torchOnImage={<FlashOnIcon width={35} height={35} />}
-                    torchOffImage={<FlashOffIcon width={35} height={35} />}
-                    torchImageStyle={{
-                        marginTop: 8,
-                        marginRight: 5,
-                        borderColor: 'red'
-                    }}
-                />
-
-                <View style={styles.actionOverlay}>
-                    <TouchableOpacity onPress={this.handleOpenGallery}>
-                        <GalleryIcon width={50} height={50} />
-                    </TouchableOpacity>
-                </View>
-                {text !== undefined && (
-                    <Text style={styles.textOverlay}>{text}</Text>
+            <>
+                {cameraStatus !== this.CameraAuthStatus.NOT_AUTHORIZED && (
+                    <View
+                        style={{
+                            flex: 1
+                        }}
+                    >
+                        <Camera
+                            style={styles.preview}
+                            scanBarcode={true}
+                            torchMode={isTorchOn ? 'on' : 'off'}
+                            onReadCode={this.onQRCodeScan}
+                        />
+                        <View style={styles.actionOverlay}>
+                            <TouchableOpacity
+                                style={styles.flashButton}
+                                onPress={this.toggleTorch}
+                            >
+                                {isTorchOn ? (
+                                    <FlashOnIcon width={35} height={35} />
+                                ) : (
+                                    <FlashOffIcon width={35} height={35} />
+                                )}
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={this.handleOpenGallery}>
+                                <GalleryIcon width={50} height={50} />
+                            </TouchableOpacity>
+                        </View>
+                        {text !== undefined && (
+                            <Text style={styles.textOverlay}>{text}</Text>
+                        )}
+                        <View
+                            style={[
+                                styles.contentRow,
+                                { height: this.maskLength }
+                            ]}
+                        >
+                            <View style={styles.overlay} />
+                            <View style={styles.scan}>
+                                <ScanFrameSvg height="100%" />
+                            </View>
+                            <View style={styles.overlay} />
+                        </View>
+                        <View
+                            style={{
+                                flex: 1,
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Button
+                                title={localeString('general.cancel')}
+                                onPress={() => goBack()}
+                                iconOnly
+                                noUppercase
+                            />
+                        </View>
+                    </View>
                 )}
 
-                <View style={[styles.contentRow, { height: this.maskLength }]}>
-                    <View style={styles.overlay} />
-                    <View style={styles.scan}>
-                        <ScanFrameSvg height="100%" />
+                {cameraStatus === this.CameraAuthStatus.NOT_AUTHORIZED && (
+                    <View style={styles.content}>
+                        <Text
+                            style={{
+                                fontFamily: 'Lato-Regular',
+                                textAlign: 'center',
+                                padding: 15
+                            }}
+                        >
+                            {localeString(
+                                'components.QRCodeScanner.noCameraAccess'
+                            )}
+                        </Text>
+                        <Button
+                            title={localeString('general.goBack')}
+                            onPress={() => goBack()}
+                            secondary
+                            containerStyle={{ width: 200 }}
+                            adaptiveWidth
+                        />
                     </View>
-                    <View style={styles.overlay} />
-                </View>
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <Button
-                        title={localeString('general.cancel')}
-                        onPress={() => goBack()}
-                        iconOnly
-                        noUppercase
-                    />
-                </View>
-            </View>
+                )}
+            </>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    preview: {
         flex: 1
     },
     flashButton: {
@@ -178,6 +264,7 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)'
     }
 });
