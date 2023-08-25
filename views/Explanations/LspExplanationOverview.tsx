d490e630053d9fd8fdbeadfa6c143bd1b39acd88
Evan Kaloudis <zeusln@tutanota.com>
2023-08-24 21:05:38 -0400

    LSP: create LSP Overview and Wrapped Invoice explanation pages

---
+++ views/Explanations/LspExplanationOverview.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Text, ScrollView } from 'react-native';
+
+import NavigationService from '../../NavigationService';
+import Button from '../../components/Button';
+import Header from '../../components/Header';
+import Screen from '../../components/Screen';
+
+import { localeString } from '../../utils/LocaleUtils';
+import { themeColor } from '../../utils/ThemeUtils';
+
+interface LspExplanationOverviewProps {
+    navigation: any;
+}
+
+const FONT_SIZE = 17;
+
+export default class LspExplanationOverview extends React.PureComponent<
+    LspExplanationOverviewProps,
+    {}
+> {
+    render() {
+        const { navigation } = this.props;
+        return (
+            <Screen>
+                <Header
+                    leftComponent="Back"
+                    centerComponent={{
+                        text: localeString(
+                            'views.LspExplanationOverview.title'
+                        ),
+                        style: {
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular'
+                        }
+                    }}
+                    navigation={navigation}
+                />
+                <ScrollView style={{ margin: 20 }}>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE
+                        }}
+                    >
+                        {localeString('views.LspExplanationOverview.text1')}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE
+                        }}
+                    >
+                        {localeString('views.LspExplanationOverview.text2')}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step1'
+                        )}`}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step2'
+                        )}`}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step3'
+                        )}`}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step4'
+                        )}`}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step5'
+                        )}`}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {`\u2022 ${localeString(
+                            'views.LspExplanationOverview.step6'
+                        )}`}
+                    </Text>
+                    <Button
+                        title={localeString(
+                            'views.LspExplanationOverview.buttonText'
+                        )}
+                        onPress={() =>
+                            NavigationService.navigate(
+                                'LspExplanationWrappedInvoices'
+                            )
+                        }
+                    />
+                </ScrollView>
+            </Screen>
+        );
+    }
+}
---
+++ views/Explanations/LspExplanationWrappedInvoices.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Text, ScrollView } from 'react-native';
+import Screen from '../../components/Screen';
+import Header from '../../components/Header';
+
+import { localeString } from '../../utils/LocaleUtils';
+import { themeColor } from '../../utils/ThemeUtils';
+
+interface LspExplanationWrappedInvoicesProps {
+    navigation: any;
+}
+
+const FONT_SIZE = 18;
+
+export default class LspExplanationWrappedInvoices extends React.PureComponent<
+    LspExplanationWrappedInvoicesProps,
+    {}
+> {
+    render() {
+        const { navigation } = this.props;
+        return (
+            <Screen>
+                <Header
+                    leftComponent="Back"
+                    centerComponent={{
+                        text: localeString(
+                            'views.LspExplanationWrappedInvoices.title'
+                        ),
+                        style: {
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular'
+                        }
+                    }}
+                    navigation={navigation}
+                />
+                <ScrollView style={{ margin: 20 }}>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE
+                        }}
+                    >
+                        {localeString(
+                            'views.LspExplanationWrappedInvoices.text1'
+                        )}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {localeString(
+                            'views.LspExplanationWrappedInvoices.text2'
+                        )}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {localeString(
+                            'views.LspExplanationWrappedInvoices.text3'
+                        )}
+                    </Text>
+                    <Text
+                        style={{
+                            color: themeColor('text'),
+                            fontFamily: 'Lato-Regular',
+                            fontSize: FONT_SIZE,
+                            marginBottom: FONT_SIZE + 10
+                        }}
+                    >
+                        {localeString(
+                            'views.LspExplanationWrappedInvoices.text4'
+                        )}
+                    </Text>
+                </ScrollView>
+            </Screen>
+        );
+    }
+}
