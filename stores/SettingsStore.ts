d5fbdd7d2b396a27ae0eececc8cb55b9e5084a68
Evan Kaloudis <zeusln@tutanota.com>
2023-09-26 10:51:35 -0400

    Settings: dontAllowOtherPeers: force toggle on for now

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -904,6 +904,15 @@ export default class SettingsStore {
                     this.settings.fiatEnabled = true;
                 }
 
+                // TODO PEGASUS
+                // temporarily toggle all alpha users settings for now
+                const MOD_KEY_1 = 'neutrino-mod-1';
+                const mod1 = await EncryptedStorage.getItem(MOD_KEY_1);
+                if (!mod1) {
+                    this.settings.dontAllowOtherPeers = true;
+                    await EncryptedStorage.setItem(MOD_KEY_1, 'true');
+                }
+
                 // set default LSPs if not defined
                 if (this.settings.enableLSP === undefined) {
                     this.settings.enableLSP = true;
