d9e555dc0ba41ec1c9abc8d3b01c0698f938fd69
Evan Kaloudis <zeusln@tutanota.com>
2023-09-29 13:20:27 -0400

    refactor: NodeConfiguration: createNewWallet

--- views/Settings/NodeConfiguration.tsx
+++ views/Settings/NodeConfiguration.tsx
@@ -529,6 +529,50 @@ export default class NodeConfiguration extends React.Component<
         navigation.navigate('Wallet', { refresh: true });
     };
 
+    createNewWallet = async (network: string = 'Mainnet') => {
+        const { recoveryCipherSeed, channelBackupsBase64 } = this.state;
+        const { SettingsStore, navigation } = this.props;
+        const { updateSettings } = SettingsStore;
+
+        this.setState({
+            creatingWallet: true
+        });
+
+        const response = await createLndWallet(
+            recoveryCipherSeed,
+            undefined,
+            true,
+            channelBackupsBase64
+        );
+
+        const { wallet, seed, randomBase64 }: any = response;
+
+        if (wallet && wallet.admin_macaroon) {
+            this.setState({
+                adminMacaroon: wallet.admin_macaroon,
+                seedPhrase: seed.cipher_seed_mnemonic,
+                walletPassword: randomBase64,
+                embeddedLndNetwork: network,
+                creatingWallet: false
+            });
+
+            this.saveNodeConfiguration(true);
+
+            if (recoveryCipherSeed) {
+                await updateSettings({
+                    recovery: true
+                });
+            }
+
+            navigation.navigate('Wallet');
+        } else {
+            this.setState({
+                creatingWallet: false,
+                errorCreatingWallet: true
+            });
+        }
+    };
+
     render() {
         const { navigation, SettingsStore } = this.props;
         const {
@@ -571,8 +615,7 @@ export default class NodeConfiguration extends React.Component<
             createAccountError,
             createAccountSuccess,
             createAccount,
-            seedPhrase,
-            updateSettings
+            seedPhrase
         } = SettingsStore;
 
         const supportsTor =
@@ -1431,70 +1474,7 @@ export default class NodeConfiguration extends React.Component<
                                                       )
                                             }
                                             onPress={async () => {
-                                                this.setState({
-                                                    creatingWallet: true
-                                                });
-
-                                                let response;
-
-                                                try {
-                                                    response =
-                                                        await createLndWallet(
-                                                            recoveryCipherSeed,
-                                                            undefined,
-                                                            undefined,
-                                                            channelBackupsBase64
-                                                        );
-                                                } catch (e) {
-                                                    this.setState({
-                                                        creatingWallet: false,
-                                                        errorCreatingWallet:
-                                                            true
-                                                    });
-                                                }
-
-                                                const {
-                                                    wallet,
-                                                    seed,
-                                                    randomBase64
-                                                }: any = response;
-
-                                                if (
-                                                    wallet &&
-                                                    wallet.admin_macaroon
-                                                ) {
-                                                    this.setState({
-                                                        adminMacaroon:
-                                                            wallet.admin_macaroon,
-                                                        seedPhrase:
-                                                            seed.cipher_seed_mnemonic,
-                                                        walletPassword:
-                                                            randomBase64,
-                                                        embeddedLndNetwork:
-                                                            'Mainnet',
-                                                        creatingWallet: false
-                                                    });
-
-                                                    this.saveNodeConfiguration(
-                                                        true
-                                                    );
-
-                                                    if (recoveryCipherSeed) {
-                                                        await updateSettings({
-                                                            recovery: true
-                                                        });
-                                                    }
-
-                                                    navigation.navigate(
-                                                        'Wallet'
-                                                    );
-                                                } else {
-                                                    this.setState({
-                                                        creatingWallet: false,
-                                                        errorCreatingWallet:
-                                                            true
-                                                    });
-                                                }
+                                                await this.createNewWallet();
                                             }}
                                             tertiary
                                         />
@@ -1511,60 +1491,9 @@ export default class NodeConfiguration extends React.Component<
                                                       )
                                             }
                                             onPress={async () => {
-                                                this.setState({
-                                                    creatingWallet: true
-                                                });
-
-                                                const response =
-                                                    await createLndWallet(
-                                                        recoveryCipherSeed,
-                                                        undefined,
-                                                        true,
-                                                        channelBackupsBase64
-                                                    );
-
-                                                const {
-                                                    wallet,
-                                                    seed,
-                                                    randomBase64
-                                                }: any = response;
-
-                                                if (
-                                                    wallet &&
-                                                    wallet.admin_macaroon
-                                                ) {
-                                                    this.setState({
-                                                        adminMacaroon:
-                                                            wallet.admin_macaroon,
-                                                        seedPhrase:
-                                                            seed.cipher_seed_mnemonic,
-                                                        walletPassword:
-                                                            randomBase64,
-                                                        embeddedLndNetwork:
-                                                            'Testnet',
-                                                        creatingWallet: false
-                                                    });
-
-                                                    this.saveNodeConfiguration(
-                                                        true
-                                                    );
-
-                                                    if (recoveryCipherSeed) {
-                                                        await updateSettings({
-                                                            recovery: true
-                                                        });
-                                                    }
-
-                                                    navigation.navigate(
-                                                        'Wallet'
-                                                    );
-                                                } else {
-                                                    this.setState({
-                                                        creatingWallet: false,
-                                                        errorCreatingWallet:
-                                                            true
-                                                    });
-                                                }
+                                                await this.createNewWallet(
+                                                    'Testnet'
+                                                );
                                             }}
                                             tertiary
                                         />
