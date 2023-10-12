dcfdf64534d2debef5ab3c7f011d4b5bde98169f
Evan Kaloudis <zeusln@tutanota.com>
2023-10-12 11:31:16 -0400

    Lightning address: refactor push notification registration

--- views/Wallet/Wallet.tsx
+++ views/Wallet/Wallet.tsx
@@ -391,6 +391,12 @@ export default class Wallet extends React.Component<WalletProps, WalletState> {
                 if (lightningAddress.automaticallyAccept) {
                     LightningAddressStore.prepareToAutomaticallyAccept();
                 }
+
+                if (
+                    SettingsStore.settings.lightningAddress?.notifications === 1
+                ) {
+                    LightningAddressStore.updatePushCredentials();
+                }
             }
         } else if (implementation === 'lndhub') {
             if (connecting) {
