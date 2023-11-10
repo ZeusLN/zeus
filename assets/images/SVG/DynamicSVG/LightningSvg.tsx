e496e8144b0e4dc4941395a670e810e9cd95cd5f
myxmaster <sebi.mueller.83@gmail.com>
2023-11-10 21:56:18 +0100

    replace swipeable row icons

--- assets/images/SVG/DynamicSVG/LightningSvg.tsx
+++ assets/images/SVG/DynamicSVG/LightningSvg.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Svg, Circle, Path } from 'react-native-svg';
+import { Svg, Circle, Polygon } from 'react-native-svg';
 import { themeColor } from '../../../../utils/ThemeUtils';
 
 export default function LightningSvg() {
     const svgProps = {
-        width: '50',
-        height: '50',
+        width: '70',
+        height: '70',
         viewBox: '0 0 50 50',
         fill: 'none',
         xmlns: 'http://www.w3.org/2000/svg'
@@ -18,8 +18,13 @@ export default function LightningSvg() {
         fill: '#242930'
     };
 
-    const pathProps = {
-        d: 'M16.6992 26.7087C16.6992 26.7087 18.4423 19 22.1217 14.0685L34.6992 13.5391C34.6992 13.5391 28.5722 19.2803 26.7978 22.395L34.1356 21.8499C34.1356 21.8499 22.5548 30.1192 18.6615 38.5391C18.6615 38.5391 17.8161 31.609 21.7876 26.1792L16.6992 26.7087Z',
+    const polygon1Props = {
+        points: '20.802,29.826 23.896,36.001 25.676,32.449 24.362,29.826 25.676,27.2 23.896,23.651',
+        fill: themeColor('bolt')
+    };
+
+    const polygon2Props = {
+        points: '29.197,20.173 26.103,14 24.323,17.55 25.637,20.173 24.323,22.799 26.103,26.351',
         fill: themeColor('bolt')
     };
 
@@ -27,6 +32,7 @@ export default function LightningSvg() {
         Svg,
         svgProps,
         React.createElement(Circle, circleProps),
-        React.createElement(Path, pathProps)
+        React.createElement(Polygon, polygon1Props),
+        React.createElement(Polygon, polygon2Props)
     );
 }
