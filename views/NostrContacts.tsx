49383b5611d2385b00658dbaf65fb7c8b9df1337
shubham <shubham@zeusln.com>
2024-01-18 14:28:07 +0530

    Add ContactUtils and lock version of react-native-fs

--- views/NostrContacts.tsx
+++ views/NostrContacts.tsx
@@ -9,11 +9,9 @@ import {
     Animated,
     Easing
 } from 'react-native';
-import { v4 as uuidv4 } from 'uuid';
 import { CheckBox, Icon } from 'react-native-elements';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { relayInit, nip05, nip19 } from 'nostr-tools';
-import RNFS from 'react-native-fs';
 
 import Button from '../components/Button';
 import Header from '../components/Header';
@@ -24,6 +22,7 @@ import { ErrorMessage } from '../components/SuccessErrorMessage';
 import { Row } from '../components/layout/Row';
 
 import AddressUtils from '../utils/AddressUtils';
+import ContactUtils from '../utils/ContactUtils';
 import { localeString } from '../utils/LocaleUtils';
 import { themeColor } from '../utils/ThemeUtils';
 
@@ -197,86 +196,6 @@ export default class NostrContacts extends React.Component<
         });
     }
 
-    transformContactData = async (contact: any) => {
-        try {
-            const name = contact?.display_name || contact?.name || '';
-            const transformedContact = {
-                photo: '',
-                name,
-                description: contact?.about || '',
-                lnAddress: contact?.lud16
-                    ? [contact?.lud16]
-                    : contact?.lud06
-                    ? [contact?.lud06]
-                    : contact?.lud16 && contact?.lud06
-                    ? [contact?.lud06, contact?.lud16]
-                    : [],
-                onchainAddress: [''],
-                pubkey: [''],
-                nip05: contact?.nip05 ? [contact?.nip05] : [],
-                nostrNpub: contact?.npub
-                    ? [contact?.npub]
-                    : contact?.pubkey
-                    ? [nip19.npubEncode(contact.pubkey)]
-                    : [],
-                contactId: uuidv4(),
-                isFavourite: false,
-                banner: '',
-                isSelected: false
-            };
-
-            if (contact?.banner) {
-                console.log('Downloading banner...');
-                const bannerFileName =
-                    'nostrContactBanner_' + transformedContact.name + '.png';
-                const bannerFilePath =
-                    RNFS.DocumentDirectoryPath + '/' + bannerFileName;
-
-                try {
-                    // Download the banner and save it locally
-                    await RNFS.downloadFile({
-                        fromUrl: contact?.banner,
-                        toFile: bannerFilePath
-                    }).promise;
-
-                    console.log('Banner download successful!');
-                    transformedContact.banner = 'file://' + bannerFilePath;
-                } catch (bannerError) {
-                    console.error('Error downloading banner:', bannerError);
-                }
-            }
-
-            console.log('Transformed contact:', transformedContact);
-
-            if (contact?.picture) {
-                console.log('Downloading image...');
-                const fileName =
-                    'nostrContactPhoto_' +
-                    transformedContact.contactId +
-                    '.png';
-                const filePath = RNFS.DocumentDirectoryPath + '/' + fileName;
-
-                try {
-                    // Download the image and save it locally
-                    await RNFS.downloadFile({
-                        fromUrl: contact?.picture,
-                        toFile: filePath
-                    }).promise;
-
-                    console.log('Download successful!');
-                    transformedContact.photo = 'file://' + filePath;
-                } catch (photoError) {
-                    console.error('Error downloading photo:', photoError);
-                }
-            }
-
-            return transformedContact;
-        } catch (error) {
-            console.error('Error transforming contact:', error);
-            return null;
-        }
-    };
-
     toggleContactSelection = (contact: any) => {
         this.setState((prevState) => {
             const selectedContacts = [...prevState.selectedContacts];
@@ -345,7 +264,8 @@ export default class NostrContacts extends React.Component<
                         this.toggleContactSelection(item);
                     } else {
                         navigation.navigate('ContactDetails', {
-                            nostrContact: await this.transformContactData(item),
+                            nostrContact:
+                                await ContactUtils.transformContactData(item),
                             isNostrContact: true
                         });
                     }
@@ -453,7 +373,7 @@ export default class NostrContacts extends React.Component<
 
             // Transform Nostr contacts data to match the format in AddContact
             const transformedContactsPromises = contactsToImport.map(
-                (contact) => this.transformContactData(contact)
+                (contact) => ContactUtils.transformContactData(contact)
             );
 
             const transformedContacts = await Promise.all(
