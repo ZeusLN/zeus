d794fc030f56aff487f9bd8cddee3c60a77cb5ef
Evan Kaloudis <zeusln@tutanota.com>
2024-03-23 09:39:46 -0400

    Send: lookup Nostr contacts

--- utils/handleAnything.ts
+++ utils/handleAnything.ts
@@ -10,10 +10,82 @@ import NodeUriUtils from './NodeUriUtils';
 import { localeString } from './LocaleUtils';
 import BackendUtils from './BackendUtils';
 
+// Nostr
+import { DEFAULT_NOSTR_RELAYS } from '../stores/SettingsStore';
+import { relayInit, nip05, nip19 } from 'nostr-tools';
+import ContactUtils from './ContactUtils';
+
 const { nodeInfoStore, invoicesStore, unitsStore, settingsStore } = stores;
 
 const isClipboardValue = (data: string) =>
     handleAnything(data, undefined, true);
+
+const attemptNip05Lookup = async (data: string) => {
+    try {
+        const lookup: any = await nip05.queryProfile(data);
+        const pubkey = lookup.pubkey;
+        return await nostrProfileLookup(pubkey);
+    } catch (e) {
+        throw new Error(localeString('utils.handleAnything.addressError'));
+    }
+};
+
+const nostrProfileLookup = async (data: string) => {
+    let profile: any;
+
+    const pubkey = data;
+    const profilesEventsPromises = DEFAULT_NOSTR_RELAYS.map(
+        async (relayItem) => {
+            const relay = relayInit(relayItem);
+            relay.on('connect', () => {
+                console.log(`connected to ${relay.url}`);
+            });
+            relay.on('error', () => {
+                console.log(`failed to connect to ${relay.url}`);
+            });
+
+            await relay.connect();
+            return relay.list([
+                {
+                    authors: [pubkey],
+                    kinds: [0]
+                }
+            ]);
+        }
+    );
+
+    await Promise.all(profilesEventsPromises).then((profilesEventsArrays) => {
+        const profileEvents = profilesEventsArrays
+            .flat()
+            .filter((event) => event !== undefined);
+
+        profileEvents.forEach((item: any) => {
+            try {
+                const content = JSON.parse(item.content);
+                if (!profile || item.created_at > profile.timestamp) {
+                    profile = {
+                        content,
+                        timestamp: item.created_at
+                    };
+                }
+            } catch (error: any) {
+                throw new Error(
+                    `Error parsing JSON for item with ID ${item.id}: ${error.message}`
+                );
+            }
+        });
+    });
+
+    return [
+        'ContactDetails',
+        {
+            nostrContact: await ContactUtils.transformContactData(
+                profile.content
+            ),
+            isNostrContact: true
+        }
+    ];
+};
 
 const handleAnything = async (
     data: string,
@@ -259,8 +331,8 @@ const handleAnything = async (
                         throw new Error(error);
                     }
                 })
-                .catch(() => {
-                    throw new Error(error);
+                .catch(async () => {
+                    return await attemptNip05Lookup(data);
                 });
         }
     } else if (value.includes('config=') && value.includes('lnd.config')) {
@@ -394,6 +466,16 @@ const handleAnything = async (
                     localeString('utils.handleAnything.invalidLnurlParams')
                 );
             });
+    } else if (AddressUtils.isValidNpub(data)) {
+        try {
+            const decoded = nip19.decode(data);
+            const pubkey = decoded.data.toString();
+            return await nostrProfileLookup(pubkey);
+        } catch (e) {
+            throw new Error(
+                localeString('utils.handleAnything.nostrProfileError')
+            );
+        }
     } else if (data.startsWith('zeuscontact:')) {
         const zeusContactData = data.replace('zeuscontact:', '');
         const contact = JSON.parse(zeusContactData);
