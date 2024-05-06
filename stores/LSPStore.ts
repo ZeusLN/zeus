ce563f0ca14277500c46c5dad89650aa98abf857
Evan Kaloudis <zeusln@tutanota.com>
2024-05-06 13:06:33 -0400

    LSPS1: REST: change get_order to a GET call

--- stores/LSPStore.ts
+++ stores/LSPStore.ts
@@ -494,21 +494,13 @@ export default class LSPStore {
     @action
     public getOrderREST(id: string) {
         this.loading = true;
-        const data = JSON.stringify({
-            order_id: id
-        });
-        const endpoint = `${this.getLSPS1Rest()}/api/v1/get_order`;
+        const endpoint = `${this.getLSPS1Rest()}/api/v1/get_order?order_id=${id}`;
 
         console.log('Sending data to:', endpoint);
 
-        return ReactNativeBlobUtil.fetch(
-            'POST',
-            endpoint,
-            {
-                'Content-Type': 'application/json'
-            },
-            data
-        )
+        return ReactNativeBlobUtil.fetch('GET', endpoint, {
+            'Content-Type': 'application/json'
+        })
             .then((response) => {
                 const responseData = JSON.parse(response.data);
                 console.log('Response received:', responseData);
