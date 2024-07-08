772cbcc400acd66e921a3f299c709d026d8c08c8
shubham <shubham@zeusln.com>
2024-07-08 23:46:31 +0530

    Update locale on LSP settings

--- views/Settings/LSP.tsx
+++ views/Settings/LSP.tsx
@@ -266,9 +266,7 @@ export default class LSP extends React.Component<LSPProps, LSPState> {
                                         color: themeColor('secondaryText')
                                     }}
                                 >
-                                    {`${localeString(
-                                        'views.Settings.LSP.lspAccessKey'
-                                    )}`}
+                                    {`${localeString('views.LSPS1.token')}`}
                                 </Text>
                                 <TextInput
                                     value={accessKey}
