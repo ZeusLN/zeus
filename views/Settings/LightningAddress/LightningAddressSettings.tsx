781427bad370daeb6fc336ff1a2b78c1c8443c44
Evan Kaloudis <zeusln@tutanota.com>
2024-12-19 16:42:04 -0500

    ZEUS-2626: Lightning Address Settings: Notifications setting does not persist

--- views/Settings/LightningAddress/LightningAddressSettings.tsx
+++ views/Settings/LightningAddress/LightningAddressSettings.tsx
@@ -72,7 +72,10 @@ export default class LightningAddressSettings extends React.Component<
                 : false,
             nostrPrivateKey: settings.lightningAddress?.nostrPrivateKey || '',
             nostrRelays: settings.lightningAddress?.nostrRelays || [],
-            notifications: settings.lightningAddress?.notifications || 1
+            notifications:
+                settings.lightningAddress?.notifications !== undefined
+                    ? settings.lightningAddress.notifications
+                    : 1
         });
     }
 
