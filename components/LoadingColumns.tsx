2deff68127d147cccdb1718884933af03ee44387
Evan Kaloudis <zeusln@tutanota.com>
2024-12-18 19:39:31 -0500

    Loading Columns: make surround wordmarks visible on light themes

--- components/LoadingColumns.tsx
+++ components/LoadingColumns.tsx
@@ -11,9 +11,63 @@ function LoadingColumns() {
             source={loader}
             autoPlay
             colorFilters={[
+                // highlighted wordmark
                 {
                     keypath: 'Layer 14 Outlines',
                     color: themeColor('highlight')
+                },
+                // surrounding wordmarks
+                {
+                    keypath: 'Layer 1 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 2 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 3 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 4 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 5 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 6 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 7 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 8 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 9 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 10 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 11 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 12 Outlines',
+                    color: themeColor('text')
+                },
+                {
+                    keypath: 'Layer 13 Outlines',
+                    color: themeColor('text')
                 }
             ]}
             resizeMode="contain"
