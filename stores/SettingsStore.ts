f93342cc4629ab19a7ac88e426f36084087db869
myxmaster <sebi.mueller.83@gmail.com>
2024-12-23 13:18:33 +0100

    use navigation param instead of comingFromLockscreen flag in SettingsStore

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -1237,7 +1237,6 @@ export default class SettingsStore {
     @observable implementation: Implementations;
     @observable certVerification: boolean | undefined;
     @observable public loggedIn = false;
-    @observable public comingFromLockscreen: boolean = false;
     @observable public connecting = true;
     @observable public lurkerExposed = false;
     private lurkerTimeout: ReturnType<typeof setTimeout> | null = null;
