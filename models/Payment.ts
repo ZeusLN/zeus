f7894f3b452f8a6cd36f5047c311330ae62a16b9
Evan Kaloudis <zeusln@tutanota.com>
2025-01-22 22:28:27 -0500

    ZEUS-2763: CLN payments incorrectly listed as 0-amt

--- models/Payment.ts
+++ models/Payment.ts
@@ -198,6 +198,7 @@ export default class Payment extends BaseModel {
             : this.value_sat ||
                   this.value ||
                   Number(this.msatoshi_sent) / 1000 ||
+                  Number(this.amount_sent_msat) / 1000 ||
                   0;
     }
 
