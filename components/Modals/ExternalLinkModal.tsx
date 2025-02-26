60a8a482447aed633874665bb21c5ed14839db1e
Abhishek <bumblebee31304@gmail.com>
2025-02-27 01:25:17 +0530

    fix: lint issues

--- components/Modals/ExternalLinkModal.tsx
+++ components/Modals/ExternalLinkModal.tsx
@@ -118,7 +118,7 @@ export default class ExternalLinkModal extends React.Component<
                         </View>
                     </View>
                 </View>
-            </ModalBox >
+            </ModalBox>
         );
     }
 }
