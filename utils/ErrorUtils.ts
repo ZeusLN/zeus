8357a79a4f71d6a0f6281feb036ed9e46a99ec13
Evan Kaloudis <zeusln@tutanota.com>
2025-03-05 22:37:54 -0500

    ErrorUtils: handle invalid response data from the server

--- utils/ErrorUtils.ts
+++ utils/ErrorUtils.ts
@@ -9,6 +9,8 @@ const userFriendlyErrors: any = {
     'Error: Unable to resolve host': 'error.nodeConnectError',
     'Error: {"code":2,"message":"verification failed: signature mismatch after caveat verification","details":[]}':
         'error.invalidMacaroon',
+    'ReactNativeBlobUtil failed to encode response data to BASE64 string.':
+        'error.invalidResponse',
     FAILURE_REASON_TIMEOUT: 'error.failureReasonTimeout',
     FAILURE_REASON_NO_ROUTE: 'error.failureReasonNoRoute',
     FAILURE_REASON_ERROR: 'error.failureReasonError',
