6f56b4a7ac2727bb9fd53594643659730f5f6db2
Shubham Kumar <shubham@zeusln.com>
2025-03-28 21:37:40 +0530

    Wiring up new hindi locale

--- utils/LocaleUtils.ts
+++ utils/LocaleUtils.ts
@@ -23,6 +23,7 @@ import * as PL from '../locales/pl.json';
 import * as HE from '../locales/he.json';
 import * as HR from '../locales/hr.json';
 import * as SW from '../locales/sw.json';
+import * as hi_EN from '../locales/hi_IN.json';
 
 // in progress
 import * as FA from '../locales/fa.json';
@@ -65,6 +66,7 @@ const Japanese: any = JA;
 const Hebrew: any = HE;
 const Croatian: any = HR;
 const Korean: any = KO;
+const Hindi: any = hi_EN;
 
 // strings that are needed on the java layer
 const JAVA_LAYER_STRINGS = [
@@ -133,6 +135,9 @@ export function localeString(localeString: string): any {
             return Croatian[localeString] || English[localeString];
         case 'ko':
             return Korean[localeString] || English[localeString];
+        case 'hi_IN':
+            return Hindi[localeString] || English[localeString];
+
         default:
             return English[localeString];
     }
