2b185420dbe4edad40e9dd310e22dce2d766660c
Evan Kaloudis <zeusln@tutanota.com>
2025-03-28 19:11:05 -0400

    fix: ZEUS Nuts create: default allowComments to true locally

--- stores/LightningAddressStore.ts
+++ stores/LightningAddressStore.ts
@@ -384,7 +384,8 @@ export default class LightningAddressStore {
             await this.settingsStore.updateSettings({
                 lightningAddress: {
                     enabled: true,
-                    automaticallyAccept: true
+                    automaticallyAccept: true,
+                    allowComments: true
                 }
             });
 
