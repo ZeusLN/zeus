67a5f4f3a96fba9a8e28578e495642833c53f553
Khushal <khushal2004a@gmail.com>
2025-04-19 07:46:47 +0530

    implemented bolt12 withdrawal request

--- views/Tools/index.tsx
+++ views/Tools/index.tsx
@@ -447,6 +447,60 @@ export default class Tools extends React.Component<ToolsProps, {}> {
                             </TouchableOpacity>
                         </View>
                     )}
+
+                    {BackendUtils.supportsInvoiceRequest() && (
+                        <View
+                            style={{
+                                backgroundColor: themeColor('secondary'),
+                                width: '90%',
+                                borderRadius: 10,
+                                alignSelf: 'center',
+                                marginVertical: 5
+                            }}
+                        >
+                            <TouchableOpacity
+                                onPress={() =>
+                                    navigation.navigate('WithdrawalRequest')
+                                }
+                            >
+                                <View style={styles.columnField}>
+                                    <View style={styles.icon}>
+                                        <Text
+                                            style={{
+                                                color: themeColor('text'),
+                                                fontSize: 14,
+                                                fontFamily:
+                                                    'PPNeueMontreal-Book'
+                                            }}
+                                        >
+                                            <Icon
+                                                name="edit"
+                                                type="feather"
+                                                color={themeColor('text')}
+                                                underlayColor="transparent"
+                                                size={17}
+                                            />
+                                        </Text>
+                                    </View>
+                                    <Text
+                                        style={{
+                                            ...styles.columnText,
+                                            color: themeColor('text')
+                                        }}
+                                    >
+                                        {localeString(
+                                            'views.Tools.withdrawal.title'
+                                        )}
+                                    </Text>
+                                    <View style={styles.ForwardArrow}>
+                                        <ForwardIcon
+                                            stroke={forwardArrowColor}
+                                        />
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
                 </ScrollView>
             </Screen>
         );
