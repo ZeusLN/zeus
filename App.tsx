ab99a0f2966a74e2173e41771397e2f432260e24
Khushal <khushal2004a@gmail.com>
2025-04-19 07:46:47 +0530

    implemented bolt12 withdrawal request

--- App.tsx
+++ App.tsx
@@ -246,6 +246,7 @@ import CashuTools from './views/Tools/CashuTools';
 import NodeConfigExportImport from './views/Tools/NodeConfigExportImport';
 
 import { isLightTheme, themeColor } from './utils/ThemeUtils';
+import WithdrawalRequest from './views/Tools/WithdrawalRequest';
 
 export default class App extends React.PureComponent {
     private backPressListenerSubscription: NativeEventSubscription;
@@ -1154,6 +1155,12 @@ export default class App extends React.PureComponent {
                                                         name="NodeConfigExportImport" // @ts-ignore:next-line
                                                         component={
                                                             NodeConfigExportImport
+                                                        }
+                                                    />
+                                                    <Stack.Screen
+                                                        name="WithdrawalRequest" // @ts-ignore:next-line
+                                                        component={
+                                                            WithdrawalRequest
                                                         }
                                                     />
                                                 </Stack.Navigator>
