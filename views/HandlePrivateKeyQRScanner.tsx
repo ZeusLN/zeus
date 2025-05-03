c8f9d699b23bd0de2f01356b9a5fc0c0ff9cc7b2
Khushal <khushal2004a@gmail.com>
2025-05-03 21:47:43 +0530

    feat(sweep): Add WIF validation and QR scanning support

--- views/HandlePrivateKeyQRScanner.tsx
+++ views/HandlePrivateKeyQRScanner.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Alert, View } from 'react-native';
 import { Header } from 'react-native-elements';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import LoadingIndicator from '../components/LoadingIndicator';
@@ -10,101 +10,57 @@ import QRCodeScanner from '../components/QRCodeScanner';
 import { localeString } from '../utils/LocaleUtils';
 import { themeColor } from '../utils/ThemeUtils';
 import NodeInfoStore from '../stores/NodeInfoStore';
-import { networks } from 'bitcoinjs-lib';
-import ECPairFactory from 'ecpair';
-import * as secp256k1 from '@noble/secp256k1';
+import SweepStore from '../stores/SweepStore';
 
 interface HandlePrivateKeyQRProps {
     navigation: StackNavigationProp<any, any>;
     NodeInfoStore: NodeInfoStore;
+    SweepStore: SweepStore;
 }
 
 interface HandlePrivateKeyQRState {
     loading: boolean;
 }
 
-const tinySecp256k1 = {
-    isPoint: (p: Uint8Array): boolean => {
-        try {
-            secp256k1.Point.fromHex(p);
-            return true;
-        } catch {
-            return false;
-        }
-    },
-    isPrivate: (d: Uint8Array): boolean => {
-        try {
-            return secp256k1.utils.isValidPrivateKey(d);
-        } catch {
-            return false;
-        }
-    },
-    pointFromScalar: (d: Uint8Array): Uint8Array | null => {
-        try {
-            const point = secp256k1.Point.fromPrivateKey(d);
-            return point.toRawBytes(true);
-        } catch {
-            return null;
-        }
-    },
-    pointCompress: (p: Uint8Array, compressed = true): Uint8Array => {
-        const point = secp256k1.Point.fromHex(p);
-        return point.toRawBytes(compressed);
-    },
-    sign: (h: Uint8Array, d: Uint8Array): Uint8Array => {
-        return secp256k1.signSync(h, d);
-    },
-    verify: (h: Uint8Array, q: Uint8Array, signature: Uint8Array): boolean => {
-        return secp256k1.verify(signature, h, q);
-    }
-};
-
+@inject('NodeInfoStore', 'SweepStore')
 @observer
 export default class HandlePrivateKeyQRScanner extends React.Component<
     HandlePrivateKeyQRProps,
     HandlePrivateKeyQRState
 > {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            loading: false
-        };
-    }
-
-    validatePrivateKey = async (wif: string) => {
-        const ECPair = ECPairFactory(tinySecp256k1);
-        const { NodeInfoStore } = this.props;
-        const { nodeInfo } = NodeInfoStore;
-        const network = nodeInfo?.isTestNet
-            ? networks.testnet
-            : networks.bitcoin;
-
-        try {
-            ECPair.fromWIF(wif, network);
-            return true;
-        } catch (err: any) {
-            throw new Error('Invalid wif format');
-        }
+    state: HandlePrivateKeyQRState = {
+        loading: false
     };
 
     handlePrivateKeyScanned = async (data: string) => {
-        const { navigation } = this.props;
+        const { navigation, SweepStore } = this.props;
 
         this.setState({ loading: true });
 
         try {
-            await this.validatePrivateKey(data);
-            navigation.navigate('Sweep', { p: data });
+            const isValid = SweepStore.isValidWif(data);
+
+            if (isValid) {
+                navigation.navigate('Wif', { p: data });
+            } else {
+                Alert.alert(
+                    localeString('general.error'),
+                    SweepStore.sweepErrorMsg ||
+                        localeString('views.Wif.invalidWif'),
+                    [
+                        {
+                            text: localeString('general.ok'),
+                            onPress: () => void 0
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
         } catch (err: any) {
             Alert.alert(
                 localeString('general.error'),
                 err.message,
-                [
-                    {
-                        text: localeString('general.ok'),
-                        onPress: () => void 0
-                    }
-                ],
+                [{ text: localeString('general.ok'), onPress: () => void 0 }],
                 { cancelable: false }
             );
         } finally {
