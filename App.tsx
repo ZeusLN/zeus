aae3b2df894d15ebb3c41357f0935f177b36924f
Khushal <khushal2004a@gmail.com>
2025-05-03 18:51:29 +0530

    feat: wif private key sweeps

--- App.tsx
+++ App.tsx
@@ -30,6 +30,7 @@ import {
     posStore,
     settingsStore,
     swapStore,
+    sweepStore,
     syncStore,
     transactionsStore,
     unitsStore,
@@ -252,6 +253,8 @@ import CreateWithdrawalRequest from './views/Tools/CreateWithdrawalRequest';
 import WithdrawalRequestView from './views/WithdrawalRequest';
 import WithdrawalRequestInfo from './views/WithdrawalRequestInfo';
 import RedeemWithdrawalRequest from './views/RedeemWithdrawalRequest';
+import WIFSweeper from './views/wif';
+import HandlePrivateKeyQRScanner from './views/HandlePrivateKeyQRScanner';
 
 export default class App extends React.PureComponent {
     private backPressListenerSubscription: NativeEventSubscription;
@@ -307,6 +310,7 @@ export default class App extends React.PureComponent {
                 TransactionsStore={transactionsStore}
                 UnitsStore={unitsStore}
                 UTXOsStore={utxosStore}
+                SweepStore={sweepStore}
             >
                 <AppContainer>
                     <PushNotificationManager>
@@ -634,6 +638,12 @@ export default class App extends React.PureComponent {
                                                         name="HandleAnythingQRScanner" // @ts-ignore:next-line
                                                         component={
                                                             HandleAnythingQRScanner
+                                                        }
+                                                    />
+                                                    <Stack.Screen
+                                                        name="HandlePrivateKeyQRScanner" // @ts-ignore:next-line
+                                                        component={
+                                                            HandlePrivateKeyQRScanner
                                                         }
                                                     />
                                                     <Stack.Screen
@@ -1191,6 +1201,10 @@ export default class App extends React.PureComponent {
                                                         component={
                                                             RedeemWithdrawalRequest
                                                         }
+                                                    />
+                                                    <Stack.Screen
+                                                        name="WIFSweeper" // @ts-ignore:next-line
+                                                        component={WIFSweeper}
                                                     />
                                                 </Stack.Navigator>
                                             </NavigationContainer>
