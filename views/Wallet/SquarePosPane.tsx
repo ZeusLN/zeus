145eb9fa8bcf8b8037bdda6dd1330d9ec707e65c
Khushal <khushal2004a@gmail.com>
2025-05-09 08:32:33 +0530

    fix: adjust amount input handling for both fiat and sat values

--- views/Wallet/SquarePosPane.tsx
+++ views/Wallet/SquarePosPane.tsx
@@ -175,7 +175,12 @@ export default class SquarePosPane extends React.PureComponent<
             >
                 <TouchableHighlight
                     onPress={() => {
-                        if (getRate() === '$N/A') return;
+                        if (
+                            getRate({
+                                sats: this.props.UnitsStore?.units === 'sats'
+                            }) === '$N/A'
+                        )
+                            return;
                         navigation.navigate('Order', {
                             order: item
                         });
@@ -325,7 +330,9 @@ export default class SquarePosPane extends React.PureComponent<
             <View style={{ flex: 1 }}>
                 <WalletHeader title={headerString} navigation={navigation} />
 
-                {getRate() === '$N/A' ? (
+                {getRate({
+                    sats: this.props.UnitsStore?.units === 'sats'
+                }) === '$N/A' ? (
                     <Animated.View
                         style={{
                             alignSelf: 'center',
@@ -348,16 +355,26 @@ export default class SquarePosPane extends React.PureComponent<
                         <Text
                             style={{
                                 color:
-                                    getRate() === '$N/A'
+                                    getRate({
+                                        sats:
+                                            this.props.UnitsStore?.units ===
+                                            'sats'
+                                    }) === '$N/A'
                                         ? themeColor('error')
                                         : themeColor('text'),
                                 alignSelf: 'center',
                                 marginBottom: 10
                             }}
                         >
-                            {getRate() === '$N/A'
+                            {getRate({
+                                sats: this.props.UnitsStore?.units === 'sats'
+                            }) === '$N/A'
                                 ? localeString('general.fiatFetchError')
-                                : getRate()}
+                                : getRate({
+                                      sats:
+                                          this.props.UnitsStore?.units ===
+                                          'sats'
+                                  })}
                         </Text>
                     </TouchableOpacity>
                 )}
