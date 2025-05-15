c3be07cc885c48709fe4591e4dc7e5b01ef489c1
Shubham Kumar <shubham@zeusln.com>
2025-05-15 21:05:41 +0530

    Add ability to donate to ZEUS

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -54,6 +54,7 @@ interface PrivacySettings {
     clipboard?: boolean;
     lurkerMode?: boolean;
     enableMempoolRates?: boolean;
+    enableDonations?: boolean;
 }
 
 interface DisplaySettings {
@@ -1182,7 +1183,8 @@ export default class SettingsStore {
             customBlockExplorer: '',
             clipboard: false,
             lurkerMode: false,
-            enableMempoolRates: true
+            enableMempoolRates: true,
+            enableDonations: false
         },
         display: {
             theme: DEFAULT_THEME,
