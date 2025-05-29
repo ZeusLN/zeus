a54a3fba0a1136e821f9a09edb413cae76ddd1da
Khushal <khushal2004a@gmail.com>
2025-05-29 22:26:20 +0530

    feat: added WIFUtils and generate on-chain address functionality

--- views/wif/index.tsx
+++ views/wif/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { inject, observer } from 'mobx-react';
 import { Route } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -17,6 +17,8 @@ import { ErrorMessage } from '../../components/SuccessErrorMessage';
 import Header from '../../components/Header';
 import Screen from '../../components/Screen';
 import TextInput from '../../components/TextInput';
+import LoadingIndicator from '../../components/LoadingIndicator';
+import OnchainFeeInput from '../../components/OnchainFeeInput';
 
 import { localeString } from '../../utils/LocaleUtils';
 import { themeColor } from '../../utils/ThemeUtils';
@@ -24,6 +26,8 @@ import { Text } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Scan from '../../assets/images/SVG/Scan.svg';
 import ShowHideToggle from '../../components/ShowHideToggle';
+import wifUtils from '../../utils/WIFUtils';
+import AddressUtils from '../../utils/AddressUtils';
 
 interface SweepProps {
     exitSetup: any;
@@ -41,9 +45,15 @@ interface SweepProps {
 interface SweepState {
     privateKey: string;
     hidden: boolean;
+    isValid: boolean;
+    destination: string;
+    error: string;
+    loading: boolean;
+    onChainAddressloading: boolean;
+    fee: string;
 }
 
-@inject('NodeInfoStore', 'SettingsStore', 'SweepStore')
+@inject('NodeInfoStore', 'SettingsStore', 'SweepStore', 'InvoicesStore')
 @observer
 export default class WIFSweeper extends React.Component<
     SweepProps,
@@ -51,7 +61,13 @@ export default class WIFSweeper extends React.Component<
 > {
     state = {
         privateKey: '',
-        hidden: true
+        hidden: true,
+        isValid: false,
+        destination: '',
+        error: '',
+        loading: false,
+        onChainAddressloading: false,
+        fee: ''
     };
 
     componentDidMount() {
@@ -77,13 +93,25 @@ export default class WIFSweeper extends React.Component<
     }
 
     render() {
-        const { navigation, SweepStore } = this.props;
-        const { privateKey, hidden } = this.state;
-        const { sweepError, sweepErrorMsg, loading, isValidWif } = SweepStore;
+        const { navigation, SweepStore, InvoicesStore } = this.props;
+        const {
+            privateKey,
+            hidden,
+            destination,
+            error,
+            loading,
+            onChainAddressloading,
+            fee
+        } = this.state;
+        const { sweepError, sweepErrorMsg } = SweepStore;
 
         const ScanButton = () => (
             <TouchableOpacity
-                onPress={() => navigation.navigate('HandlePrivateKeyQRScanner')}
+                onPress={() =>
+                    navigation.navigate('HandleAnythingQRScanner', {
+                        wif: true
+                    })
+                }
             >
                 <Scan fill={themeColor('text')} width={30} height={30} />
             </TouchableOpacity>
@@ -105,11 +133,12 @@ export default class WIFSweeper extends React.Component<
                         rightComponent={<ScanButton />}
                     />
 
-                    {sweepError && (
+                    {(sweepError || error) && (
                         <View>
                             <ErrorMessage
                                 message={
-                                    sweepErrorMsg || 'Invalid wif priv key'
+                                    sweepErrorMsg ||
+                                    localeString('views.Wif.invalidWif')
                                 }
                             />
                         </View>
@@ -148,18 +177,137 @@ export default class WIFSweeper extends React.Component<
                         </View>
                     </View>
 
-                    <View style={{ padding: 20, paddingBottom: 30 }}>
+                    <View>
+                        <View
+                            style={{ position: 'relative', marginBottom: 10 }}
+                        >
+                            <TextInput
+                                onChangeText={async (text: string) => {
+                                    let isValid;
+                                    isValid = text
+                                        ? AddressUtils.isValidBitcoinAddress(
+                                              text,
+                                              true
+                                          )
+                                        : false;
+                                    this.setState({
+                                        destination: text,
+                                        isValid,
+                                        error: ''
+                                    });
+                                }}
+                                placeholder={
+                                    onChainAddressloading
+                                        ? ''
+                                        : localeString(
+                                              'views.Wif.destinationOnChainAddress'
+                                          )
+                                }
+                                style={{
+                                    marginHorizontal: 20
+                                }}
+                                value={destination}
+                            />
+                            {onChainAddressloading && (
+                                <View style={styles.loadingOverlay}>
+                                    <LoadingIndicator />
+                                </View>
+                            )}
+                        </View>
+
+                        <View
+                            style={{
+                                paddingBottom: 30
+                            }}
+                        >
+                            <Button
+                                onPress={async () => {
+                                    try {
+                                        this.setState({
+                                            destination: '',
+                                            onChainAddressloading: true
+                                        });
+                                        await this.props.SweepStore.fetchOnChainBalance(
+                                            this.state.privateKey
+                                        );
+                                        await InvoicesStore.createUnifiedInvoice(
+                                            {
+                                                memo: '',
+                                                value:
+                                                    SweepStore.onChainBalance ??
+                                                    '10',
+                                                expiry: '3600'
+                                            }
+                                        );
+                                        if (InvoicesStore.onChainAddress) {
+                                            this.setState({
+                                                destination:
+                                                    InvoicesStore.onChainAddress,
+                                                error: '',
+                                                isValid: true,
+                                                onChainAddressloading: false
+                                            });
+                                        } else {
+                                            this.setState({
+                                                error: localeString(
+                                                    'views.Swaps.generateOnchainAddressFailed'
+                                                ),
+                                                onChainAddressloading: false
+                                            });
+                                        }
+                                    } catch (e: any) {
+                                        this.setState({
+                                            error: localeString(
+                                                'views.Swaps.generateOnchainAddressFailed'
+                                            ),
+                                            onChainAddressloading: false
+                                        });
+                                    }
+                                }}
+                                title={localeString(
+                                    'views.Swaps.generateOnchainAddress'
+                                )}
+                                secondary
+                                disabled={!privateKey || !!destination}
+                            />
+                        </View>
+                        <View
+                            style={{ marginHorizontal: 20, paddingBottom: 10 }}
+                        >
+                            <Text
+                                style={{
+                                    ...styles.text,
+                                    color: themeColor('secondaryText')
+                                }}
+                            >
+                                {localeString('views.Send.feeSatsVbyte')}
+                            </Text>
+                            <OnchainFeeInput
+                                fee={fee}
+                                onChangeFee={(text: string) =>
+                                    this.setState({ fee: text })
+                                }
+                                navigation={navigation}
+                            />
+                        </View>
                         <Button
                             title={localeString('views.Wif.sweep')}
                             onPress={() => {
-                                const valid = this.props.SweepStore.isValidWif(
+                                const { isValid, error } = wifUtils.validateWIF(
                                     this.state.privateKey
                                 );
-                                if (valid) {
-                                    isValidWif(this.state.privateKey);
+                                if (isValid) {
+                                    // Continue with sweep process
+                                    // this.props.SweepStore.sweepBtc(this.state.privateKey);
+                                } else {
+                                    this.props.SweepStore.sweepError = true;
+                                    this.props.SweepStore.sweepErrorMsg =
+                                        error ||
+                                        localeString('views.Wif.invalidWif');
                                 }
                             }}
-                            disabled={!privateKey || loading}
+                            disabled={!privateKey || loading || !destination}
+                            containerStyle={{ paddingBottom: 30 }}
                         />
                     </View>
                 </View>
@@ -167,3 +315,18 @@ export default class WIFSweeper extends React.Component<
         );
     }
 }
+
+const styles = StyleSheet.create({
+    loadingOverlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'PPNeueMontreal-Book'
+    }
+});
