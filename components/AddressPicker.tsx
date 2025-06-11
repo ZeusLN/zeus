e6be61ea6615fd8daf1d1753408aa73d44a9ddb2
Evan Kaloudis <zeusln@tutanota.com>
2025-06-11 00:21:45 -0400

    fix: fix sort labels on iOS

--- components/AddressPicker.tsx
+++ components/AddressPicker.tsx
@@ -476,7 +476,9 @@ export default class AddressPicker extends React.Component<
                                         title={localeString('general.sorting')}
                                         values={Object.keys(SortBy).map(
                                             (s) => ({
-                                                key: s,
+                                                key: localeString(
+                                                    `views.OnChainAddresses.sortBy.${s}`
+                                                ),
                                                 translateKey: `views.AddressPicker.sortBy.${s}`,
                                                 value: s
                                             })
