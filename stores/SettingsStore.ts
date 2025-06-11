7b46f1e5e796880f4c7e07c12ca7b766a3c5a6ed
Shubham Kumar <shubham@zeusln.com>
2025-06-11 19:14:18 +0530

    enhancement: add default donation percent for both lightning and cashu payments

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -95,6 +95,7 @@ interface PaymentsSettings {
     preferredMempoolRate?: string;
     slideToPayThreshold: number;
     enableDonations?: boolean;
+    defaultDonationPercentage?: number;
 }
 
 interface InvoicesSettings {
@@ -1215,7 +1216,8 @@ export default class SettingsStore {
             timeoutSeconds: '60',
             preferredMempoolRate: 'fastestFee',
             slideToPayThreshold: DEFAULT_SLIDE_TO_PAY_THRESHOLD,
-            enableDonations: false
+            enableDonations: false,
+            defaultDonationPercentage: 5
         },
         invoices: {
             addressType: '0',
