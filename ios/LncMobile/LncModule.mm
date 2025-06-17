68b2688a7456ba73cf1463872637e741ce548749
Evan Kaloudis <zeusln@tutanota.com>
2025-06-17 18:48:23 -0400

    WIP

--- ios/LncMobile/LncModule.mm
+++ ios/LncMobile/LncModule.mm
@@ -313,12 +313,12 @@ RCT_EXPORT_METHOD(createRefundTransaction:(NSString *)endpoint
 }
 
 // Don't compile this code when we build for the old architecture.
-#ifdef RCT_NEW_ARCH_ENABLED
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeLncRnSpecJSI>(params);
-}
-#endif
+//#ifdef RCT_NEW_ARCH_ENABLED
+//- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+//    (const facebook::react::ObjCTurboModule::InitParams &)params
+//{
+//    return std::make_shared<facebook::react::NativeLncRnSpecJSI>(params);
+//}
+//#endif
 
 @end
