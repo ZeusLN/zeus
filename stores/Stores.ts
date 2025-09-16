39b85fa8f9458656eb2fabfb59497c371f37d675
ajaysehwal <ajaysehwal000@gmail.com>
2025-09-16 21:48:14 +0530

    enhancement: added handoff logic for ios when app is going close

--- stores/Stores.ts
+++ stores/Stores.ts
@@ -95,5 +95,6 @@ export const nostrWalletConnectStore = new NostrWalletConnectStore(
     transactionsStore,
     cashuStore,
     invoicesStore,
-    messageSignStore
+    messageSignStore,
+    lightningAddressStore
 );
