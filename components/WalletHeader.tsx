6b60d4edc8625bb0521814435d2db8ff48426d55
Devi Charan <chitluridevicharan@gmail.com>
2025-10-27 22:30:16 +0530

    feat: AutoPay integration with Send view and wallet header

--- components/WalletHeader.tsx
+++ components/WalletHeader.tsx
@@ -183,125 +183,18 @@ const MenuBadge = ({
     </TouchableOpacity>
 );
 
-const checkAutoPayAndProcess = async (
-    invoice: string,
-    navigation: StackNavigationProp<any, any>,
-    settingsStore: SettingsStore,
-    transactionsStore: TransactionsStore
-) => {
-    try {
-        const decodedInvoice = await BackendUtils.decodePaymentRequest([
-            invoice
-        ]);
-
-        if (!decodedInvoice) {
-            const response = await handleAnything(invoice);
-            const [route, props] = response;
-            navigation.navigate(route, props);
-            return;
-        }
-
-        const getAmountFromDecodedInvoice = (decodedInvoice: any): number => {
-            // LND format (with underscores)
-
-            if (decodedInvoice.num_satoshis) {
-                return Number(decodedInvoice.num_satoshis);
-            }
-            if (decodedInvoice.num_msat) {
-                return Number(decodedInvoice.num_msat) / 1000;
-            }
-
-            if (decodedInvoice.numSatoshis) {
-                return Number(decodedInvoice.numSatoshis);
-            }
-            if (decodedInvoice.numMsat) {
-                return Number(decodedInvoice.numMsat) / 1000;
-            }
-
-            // Core Lightning format
-            if (decodedInvoice.amount_msat) {
-                return Number(decodedInvoice.amount_msat) / 1000;
-            }
-
-            // Other possible formats
-            if (decodedInvoice.amount) {
-                return Number(decodedInvoice.amount);
-            }
-            if (decodedInvoice.value) {
-                return Number(decodedInvoice.value);
-            }
-            if (decodedInvoice.valueSat) {
-                return Number(decodedInvoice.valueSat);
-            }
-            if (decodedInvoice.valueMsat) {
-                return Number(decodedInvoice.valueMsat) / 1000;
-            }
-
-            return 0;
-        };
-
-        const amount = getAmountFromDecodedInvoice(decodedInvoice);
-
-        const autoPayThreshold =
-            settingsStore.settings?.payments?.autoPayThreshold || 0;
-
-        if (
-            settingsStore.settings?.payments?.autoPayEnabled &&
-            amount > 0 &&
-            amount <= autoPayThreshold
-        ) {
-            transactionsStore.sendPayment({
-                payment_request: invoice
-            });
-
-            navigation.navigate('SendingLightning', {
-                enableDonations: false
-            });
-        } else {
-            const response = await handleAnything(invoice);
-            const [route, props] = response;
-            navigation.navigate(route, props);
-        }
-    } catch (error) {
-        console.error('Error checking auto-pay:', error);
-        // Normal processing
-        const response = await handleAnything(invoice);
-        const [route, props] = response;
-        navigation.navigate(route, props);
-    }
-};
-
 const ClipboardBadge = ({
     navigation,
-    clipboard,
-    settingsStore,
-    transactionsStore
+    clipboard
 }: {
     navigation: StackNavigationProp<any, any>;
     clipboard: string;
-    settingsStore: SettingsStore;
-    transactionsStore: TransactionsStore;
 }) => (
     <TouchableOpacity
         onPress={async () => {
-            const trimmedContent = clipboard.trim();
-
-            if (
-                trimmedContent.toLowerCase().startsWith('lnbc') ||
-                trimmedContent.toLowerCase().startsWith('lnbcrt') ||
-                trimmedContent.toLowerCase().startsWith('lntb')
-            ) {
-                await checkAutoPayAndProcess(
-                    trimmedContent,
-                    navigation,
-                    settingsStore,
-                    transactionsStore
-                );
-            } else {
-                const response = await handleAnything(clipboard);
-                const [route, props] = response;
-                navigation.navigate(route, props);
-            }
+            const response = await handleAnything(clipboard);
+            const [route, props] = response;
+            navigation.navigate(route, props);
         }}
     >
         <ClipboardSVG fill={themeColor('text')} width="24" height="30" />
@@ -394,7 +287,8 @@ interface WalletHeaderState {
     'PosStore',
     'SyncStore',
     'NostrWalletConnectStore',
-    'TransactionsStore'
+    'TransactionsStore',
+    'SyncStore'
 )
 @observer
 export default class WalletHeader extends React.Component<
@@ -454,8 +348,7 @@ export default class WalletHeader extends React.Component<
             ModalStore,
             PosStore,
             SyncStore,
-            NostrWalletConnectStore,
-            TransactionsStore
+            NostrWalletConnectStore
         } = this.props;
         const { sentTokens } = CashuStore!!;
         const { pendingHTLCs } = ChannelsStore!;
@@ -797,10 +690,6 @@ export default class WalletHeader extends React.Component<
                                         <ClipboardBadge
                                             navigation={navigation}
                                             clipboard={clipboard}
-                                            settingsStore={SettingsStore!}
-                                            transactionsStore={
-                                                TransactionsStore!
-                                            }
                                         />
                                     </View>
                                 )}
