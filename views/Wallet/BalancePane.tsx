c7385cdbf740793744bd8f5b2c439eabc70cf413
Khushal <khushal2004a@gmail.com>
2026-02-03 10:26:52 +0530

    refactor: extract duplicated pending balance handler to reusable function

--- views/Wallet/BalancePane.tsx
+++ views/Wallet/BalancePane.tsx
@@ -76,6 +76,15 @@ export default class BalancePane extends React.PureComponent<
         }
     }
 
+    handlePendingPress = () => {
+        const { ModalStore } = this.props;
+
+        ModalStore.toggleInfoModal({
+            title: localeString('views.Wallet.pendingBalanceIcon.title'),
+            text: localeString('views.Wallet.pendingBalanceIcon.explainer')
+        });
+    };
+
     render() {
         const {
             NodeInfoStore,
@@ -83,7 +92,6 @@ export default class BalancePane extends React.PureComponent<
             CashuStore,
             SettingsStore,
             SyncStore,
-            ModalStore,
             navigation,
             loading
         } = this.props;
@@ -128,16 +136,7 @@ export default class BalancePane extends React.PureComponent<
                             jumboText
                             toggleable
                             pending
-                            onPendingPress={() =>
-                                ModalStore.toggleInfoModal({
-                                    title: localeString(
-                                        'views.Wallet.pendingBalanceIcon.title'
-                                    ),
-                                    text: localeString(
-                                        'views.Wallet.pendingBalanceIcon.explainer'
-                                    )
-                                })
-                            }
+                            onPendingPress={this.handlePendingPress}
                         />
                         <View style={styles.conversion}>
                             <Conversion
@@ -171,16 +170,7 @@ export default class BalancePane extends React.PureComponent<
                             jumboText
                             toggleable
                             pending
-                            onPendingPress={() =>
-                                ModalStore.toggleInfoModal({
-                                    title: localeString(
-                                        'views.Wallet.pendingBalanceIcon.title'
-                                    ),
-                                    text: localeString(
-                                        'views.Wallet.pendingBalanceIcon.explainer'
-                                    )
-                                })
-                            }
+                            onPendingPress={this.handlePendingPress}
                         />
                         <View style={styles.conversionSecondary}>
                             <Conversion
