5a6367178c1378b39ccae9993efcdccabd3518ca
Shubham Kumar <shubham@zeusln.com>
2026-02-04 17:24:26 +0530

    enhancement: Turn off the LND and hide the settings while being in channel migration mode

--- views/Wallet/Wallet.tsx
+++ views/Wallet/Wallet.tsx
@@ -357,20 +357,20 @@ export default class Wallet extends React.Component<WalletProps, WalletState> {
                         migrationData.migrationStatus &&
                         migrationData.lndDir === lndDir
                     ) {
-                        this.setState({
-                            isChannelMigrating: true
-                        });
+                        this.setState({ isChannelMigrating: true });
+                        SettingsStore.setChannelMigrating(true);
                     } else {
-                        this.setState({
-                            isChannelMigrating: false
-                        });
+                        this.setState({ isChannelMigrating: false });
+                        SettingsStore.setChannelMigrating(false);
                     }
                 } catch (e) {
                     console.error('Failed to parse migration data:', e);
                     this.setState({ isChannelMigrating: false });
+                    SettingsStore.setChannelMigrating(false);
                 }
             } else {
                 this.setState({ isChannelMigrating: false });
+                SettingsStore.setChannelMigrating(false);
             }
 
             // This awaits on settings, so should await on Tor being bootstrapped before making requests
@@ -628,6 +628,16 @@ export default class Wallet extends React.Component<WalletProps, WalletState> {
                             return;
                         }
                         throw error;
+                    }
+
+                    const { isChannelMigrating } = this.state;
+                    if (isChannelMigrating) {
+                        console.log(
+                            'Wallet is in migration lock mode - skipping LND startup'
+                        );
+                        this.setState({ loading: false });
+                        setConnectingStatus(false);
+                        return;
                     }
 
                     // on initial load, do not run EGS
@@ -1209,9 +1219,10 @@ export default class Wallet extends React.Component<WalletProps, WalletState> {
                         SyncStore={SyncStore}
                         loading={loading}
                         isChannelMigrating={isChannelMigrating}
-                        onUnlock={() =>
-                            this.setState({ isChannelMigrating: false })
-                        }
+                        onUnlock={() => {
+                            this.setState({ isChannelMigrating: false });
+                            SettingsStore.setChannelMigrating(false);
+                        }}
                     />
 
                     {error && (
