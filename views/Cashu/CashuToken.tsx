f9cd26c39de7417d3bcc6bc8f48ef2f6d5d02de1
Evan Kaloudis <zeusln@tutanota.com>
2026-02-20 23:15:44 -0500

    ui: show 'Go to wallet' after token receive

--- views/Cashu/CashuToken.tsx
+++ views/Cashu/CashuToken.tsx
@@ -385,6 +385,29 @@ export default class CashuTokenView extends React.Component<
                     )}
                 </ScrollView>
                 {infoIndex === 0 &&
+                    (success || warningMessage) &&
+                    !received &&
+                    !pendingClaim && (
+                        <View style={{ bottom: 15 }}>
+                            <Button
+                                title={localeString(
+                                    'views.SendingLightning.goToWallet'
+                                )}
+                                icon={{
+                                    name: 'list',
+                                    size: 25,
+                                    color: themeColor('background')
+                                }}
+                                onPress={() => {
+                                    navigation.popTo('Wallet');
+                                }}
+                                containerStyle={{ marginTop: 15 }}
+                            />
+                        </View>
+                    )}
+                {infoIndex === 0 &&
+                    !success &&
+                    !warningMessage &&
                     !received &&
                     !pendingClaim &&
                     (!sent || (sent && !spent)) && (
@@ -458,8 +481,7 @@ export default class CashuTokenView extends React.Component<
                                             !haveMint ||
                                             errorAddingMint ||
                                             !isSupported ||
-                                            loading ||
-                                            success
+                                            loading
                                         }
                                     />
                                 </>
@@ -491,10 +513,7 @@ export default class CashuTokenView extends React.Component<
                                 }}
                                 containerStyle={{ marginTop: 15 }}
                                 disabled={
-                                    !hasOpenChannels ||
-                                    !isSupported ||
-                                    loading ||
-                                    success
+                                    !hasOpenChannels || !isSupported || loading
                                 }
                                 secondary
                             />
