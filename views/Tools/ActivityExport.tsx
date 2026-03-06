21618245bbed2860d1bae1866d7940dc9d00674a
ajaysehwal <ajaysehwal000@gmail.com>
2026-03-06 21:26:34 +0530

    fix: revert ActivityExport modalOverlay backgroundColor

--- views/Tools/ActivityExport.tsx
+++ views/Tools/ActivityExport.tsx
@@ -771,6 +771,7 @@ const styles = StyleSheet.create({
     },
     modalOverlay: {
         flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         justifyContent: 'center',
         alignItems: 'center'
     },
