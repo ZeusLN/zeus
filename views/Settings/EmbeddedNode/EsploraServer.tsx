c8616e98ff243ee8cabce11a2c2c34b04b89e3e2
Evan Kaloudis <zeusln@tutanota.com>
2026-03-16 16:37:12 -0400

    fix: localize hardcoded 'None' in EGS and Esplora server settings

--- views/Settings/EmbeddedNode/EsploraServer.tsx
+++ views/Settings/EmbeddedNode/EsploraServer.tsx
@@ -129,7 +129,8 @@ export default class EsploraServer extends React.Component<
                                 {localeString(
                                     'views.Settings.EmbeddedNode.defaultServer'
                                 )}
-                                : {defaultServer || 'None'}
+                                :{' '}
+                                {defaultServer || localeString('general.none')}
                             </Text>
                         </View>
 
