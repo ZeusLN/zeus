c456a70d61341f96b25f7e10907e2c6521f8afda
myxmaster <sebi.mueller.83@gmail.com>
2026-05-05 20:43:34 +0200

    replace elliptic with @noble/secp256k1 (CVE-2025-14505)

--- utils/SigningUtils.ts
+++ utils/SigningUtils.ts
@@ -1,11 +1,12 @@
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
+import { getPublicKey, signSync } from '@noble/secp256k1';
+import '../zeus_modules/noble_ecc'; // ensures hmacSha256Sync is configured for signSync
 
 export function getCompressedPublicKeyHex(privKey: Uint8Array): string {
-    return ec.keyFromPrivate(privKey).getPublic().encodeCompressed('hex');
+    return Buffer.from(getPublicKey(privKey, true)).toString('hex');
 }
 
 export function ecdsaSignDERHex(hash: Uint8Array, privKey: Uint8Array): string {
-    const sig = ec.sign(hash, privKey, { canonical: true });
-    return Buffer.from(sig.toDER()).toString('hex');
+    return Buffer.from(signSync(hash, privKey, { canonical: true })).toString(
+        'hex'
+    );
 }
