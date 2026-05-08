c2b8e4094f6f34d2979348a77b92131307917009
Evan Kaloudis <zeusln@tutanota.com>
2026-05-08 17:34:06 -0400

    fix: Onboarding: reset nav stack after wallet creation to prevent swipe-back re-entry

--- views/Onboarding/WalletSettings.tsx
+++ views/Onboarding/WalletSettings.tsx
@@ -119,7 +119,11 @@ export default class WalletSettings extends React.Component<
                     choosingPeers: false,
                     creatingWallet: false
                 }),
-            onSuccess: () => navigation.navigate('Wallet')
+            onSuccess: () =>
+                navigation.reset({
+                    index: 0,
+                    routes: [{ name: 'Wallet' }]
+                })
         });
     };
 
