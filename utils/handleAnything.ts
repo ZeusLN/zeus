3480931aadf71e775d67857b3bfc0c2e77e6cf43
Evan Kaloudis <zeusln@tutanota.com>
2026-05-17 20:12:22 -0400

    feat: clink: route through ChoosePaymentMethod when ecash is enabled

--- utils/handleAnything.ts
+++ utils/handleAnything.ts
@@ -269,7 +269,7 @@ const handleAnything = async (
         if (!AddressUtils.isValidNoffer(clinkNoffer)) {
             throw new Error(localeString('utils.handleAnything.invalidNoffer'));
         }
-        return ['ClinkPay', { noffer: clinkNoffer }];
+        return ['ClinkPay', { noffer: clinkNoffer, ecash }];
     } else if (offer) {
         if (isClipboardValue) return true;
         return [
@@ -395,7 +395,7 @@ const handleAnything = async (
         ];
     } else if (!hasAt && AddressUtils.isValidNoffer(value)) {
         if (isClipboardValue) return true;
-        return ['ClinkPay', { noffer: value }];
+        return ['ClinkPay', { noffer: value, ecash }];
     } else if (value.includes('clnrest://') || value.includes('clnrest+')) {
         if (isClipboardValue) return true;
         const { host, port, rune, implementation, enableTor } =
