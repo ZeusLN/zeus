7b07a57ce2f24969b9de2343f44da7e472bd4ce6
Khushal <khushal2004a@gmail.com>
2026-06-16 16:13:03 +0530

    feat: add Branta SDK and parse ZK params from BIP-21 URIs

--- utils/AddressUtils.ts
+++ utils/AddressUtils.ts
@@ -86,7 +86,7 @@ export const CUSTODIAL_LNDHUBS = [
 ];
 
 const bitcoinQrParser = (input: string, prefix: string) => {
-    let satAmount, lightning, offer, clinkNoffer;
+    let satAmount, lightning, offer, clinkNoffer, brantaId, brantaSecret;
     const btcAddressAndParams = input.split(prefix)[1];
     const [btcAddress, params] = btcAddressAndParams.split('?');
 
@@ -126,12 +126,34 @@ const bitcoinQrParser = (input: string, prefix: string) => {
         clinkNoffer = result.noffer || result.NOFFER;
     }
 
-    return [value, satAmount, lightning, offer, clinkNoffer];
+    if (result.branta_id || result.BRANTA_ID) {
+        brantaId = result.branta_id || result.BRANTA_ID;
+    }
+
+    if (result.branta_secret || result.BRANTA_SECRET) {
+        brantaSecret = result.branta_secret || result.BRANTA_SECRET;
+    }
+
+    return [
+        value,
+        satAmount,
+        lightning,
+        offer,
+        clinkNoffer,
+        brantaId,
+        brantaSecret
+    ];
 };
 
 class AddressUtils {
     processBIP21Uri = (input: string) => {
-        let value, satAmount, lightning, offer, clinkNoffer;
+        let value,
+            satAmount,
+            lightning,
+            offer,
+            clinkNoffer,
+            brantaId,
+            brantaSecret;
 
         // handle addresses prefixed with 'bitcoin:' and
         // payment requests prefixed with 'lightning:'
@@ -143,7 +165,9 @@ class AddressUtils {
                 parsedSatAmount,
                 parsedLightning,
                 parsedOffer,
-                parsedClinkNoffer
+                parsedClinkNoffer,
+                parsedBrantaId,
+                parsedBrantaSecret
             ] = bitcoinQrParser(
                 input,
                 input.includes('BITCOIN:') ? 'BITCOIN:' : 'bitcoin:'
@@ -165,6 +189,14 @@ class AddressUtils {
             if (parsedClinkNoffer) {
                 clinkNoffer = parsedClinkNoffer;
             }
+
+            if (parsedBrantaId) {
+                brantaId = parsedBrantaId;
+            }
+
+            if (parsedBrantaSecret) {
+                brantaSecret = parsedBrantaSecret;
+            }
         } else if (input.includes('lightning:')) {
             value = input.split('lightning:')[1];
         } else if (input.includes('LIGHTNING:')) {
@@ -175,7 +207,15 @@ class AddressUtils {
             value = input;
         }
 
-        return { value, satAmount, lightning, offer, clinkNoffer };
+        return {
+            value,
+            satAmount,
+            lightning,
+            offer,
+            clinkNoffer,
+            brantaId,
+            brantaSecret
+        };
     };
 
     processLNDHubAddress = (input: string) => {
