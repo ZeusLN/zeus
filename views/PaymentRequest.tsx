41752e047a548f6c0628b485e298e44b2a5bc955
Khushal <khushal2004a@gmail.com>
2026-06-18 19:02:18 +0530

    feat: add Branta verification for Lightning invoices

--- views/PaymentRequest.tsx
+++ views/PaymentRequest.tsx
@@ -16,6 +16,7 @@ import { ButtonGroup } from '@rneui/themed';
 
 import Amount from '../components/Amount';
 import AmountInput from '../components/AmountInput';
+import BrantaVerificationComponent from '../components/BrantaVerification';
 import Button from '../components/Button';
 import SwipeButton from '../components/SwipeButton';
 import Conversion from '../components/Conversion';
@@ -31,6 +32,8 @@ import Text from '../components/Text';
 import TextInput from '../components/TextInput';
 import { Row } from '../components/layout/Row';
 import { WarningMessage } from '../components/SuccessErrorMessage';
+
+import { BrantaVerification } from '../stores/BrantaStore';
 
 import BalanceStore from '../stores/BalanceStore';
 import ChannelsStore from '../stores/ChannelsStore';
@@ -70,7 +73,13 @@ const zaplockerDestinations = [
 interface InvoiceProps {
     exitSetup: any;
     navigation: NativeStackNavigationProp<any, any>;
-    route: Route<'PaymentRequest', { fromGraphSync?: boolean }>;
+    route: Route<
+        'PaymentRequest',
+        {
+            fromGraphSync?: boolean;
+            brantaVerification?: BrantaVerification | null;
+        }
+    >;
     BalanceStore: BalanceStore;
     InvoicesStore: InvoicesStore;
     SwapStore: SwapStore;
@@ -491,7 +500,8 @@ export default class PaymentRequest extends React.Component<
             SettingsStore,
             NodeInfoStore,
             TransactionsStore,
-            navigation
+            navigation,
+            route
         } = this.props;
         const {
             enableMultiPathPayment,
@@ -531,6 +541,8 @@ export default class PaymentRequest extends React.Component<
 
         const locale = SettingsStore.settings.locale;
         if (pay_req) pay_req.determineFormattedOriginalTimeUntilExpiry(locale);
+
+        const brantaVerification = route?.params?.brantaVerification;
 
         // variables cannot be destructured traditionally here
         // due to how we clear the pay_req from the store upon
@@ -997,6 +1009,12 @@ export default class PaymentRequest extends React.Component<
                                             'general.destination'
                                         )}
                                         value={destination}
+                                    />
+                                )}
+
+                                {brantaVerification && (
+                                    <BrantaVerificationComponent
+                                        verification={brantaVerification}
                                     />
                                 )}
 
