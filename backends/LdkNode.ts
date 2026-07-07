2da6e6adbd4addb3d339ffbf526716a3524af02c
Shubham Kumar <shubham@zeusln.com>
2026-07-07 11:44:06 +0530

    feat (ldk): enable watch-only accounts for the LDK backend

--- backends/LdkNode.ts
+++ backends/LdkNode.ts
@@ -2346,7 +2346,7 @@ export default class LdkNode {
     supportsCoinControl = () => true;
     supportsChannelCoinControl = () => true;
     supportsHopPicking = () => false;
-    supportsAccounts = () => false;
+    supportsAccounts = () => true;
     supportsRouting = () => false;
     supportsNodeInfo = () => true;
     supportsWithdrawalRequests = () => true;
@@ -2372,7 +2372,7 @@ export default class LdkNode {
     supportsListingOffers = () => false;
     supportsBolt12Address = () => false;
     supportsBolt11BlindedRoutes = () => false;
-    supportsAddressesWithDerivationPaths = () => false;
+    supportsAddressesWithDerivationPaths = () => true;
     supportsCustomFeeLimit = () => true;
     isLNDBased = () => false;
     supportsForwardingHistory = () => false;
